d5e0b3fca3b5f854ac663843f5e503f56d7a8bc9
l.cointrel <l.cointrel@mediactive.fr>
2021-06-22 18:25:06 +0200

    Separate auth routes register to admin routes

--- routes/web.php
+++ routes/web.php
@@ -9,41 +9,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-// Auth::routes();
+// Register Auth routes ..
+require_once('auth.php');
 
-// Authentication Routes...
-Route::get('login', 'Ludows\Adminify\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Ludows\Adminify\Http\Controllers\Auth\LoginController@login');
-Route::post('logout', 'Ludows\Adminify\Http\Controllers\Auth\LoginController@logout')->name('logout');
-
-// Registration Routes...
-Route::get('register', 'Ludows\Adminify\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Ludows\Adminify\Http\Controllers\Auth\RegisterController@register');
-
-// Password Reset Routes...
-Route::get('password/reset', 'Ludows\Adminify\Http\Controllers\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Ludows\Adminify\Http\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Ludows\Adminify\Http\Controllers\Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Ludows\Adminify\Http\Controllers\Auth\ResetPasswordController@reset')->name('password.update');
-
-// Confirm Password (added in v6.2)
-Route::get('password/confirm', 'Ludows\Adminify\Http\Controllers\Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
-Route::post('password/confirm', 'Ludows\Adminify\Http\Controllers\Auth\ConfirmPasswordController@confirm');
-
-// Email Verification Routes...
-Route::get('email/verify', 'Ludows\Adminify\Http\Controllers\Auth\VerificationController@show')->name('verification.notice');
-Route::get('email/verify/{id}/{hash}', 'Ludows\Adminify\Http\Controllers\Auth\VerificationController@verify')->name('verification.verify'); // v6.x
-/* Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify'); // v5.x */
-Route::get('email/resend', 'Ludows\Adminify\Http\Controllers\Auth\VerificationController@resend')->name('verification.resend');
-
-// Route::match(['get', 'post'], 'register', function () {
-//     return abort(403, 'Forbidden');
-// })->name('register');
+// Register Admin Routes..
 require_once('admin.php');
 
+// Define it'smultilang according to config site
 if(config('site-settings.multilang')) {
     require_once('multilang.php');
 }
--- src/Http/Controllers/Auth/LoginController.php
+++ src/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,12 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function showLoginForm()
+
+    {
+
+        return view('adminify::auth.login');
+
+    }
 }
