ff9c516a803c350b02463d611e73274f08d2ce74
l.cointrel <l.cointrel@mediactive.fr>
2021-06-25 09:34:13 +0200

    ResetPasswordController

--- src/Http/Controllers/Auth/ResetPasswordController.php
+++ src/Http/Controllers/Auth/ResetPasswordController.php
@@ -27,4 +27,15 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function showResetForm(Request $request)
+
+    {
+        $token = $request->route()->parameter('token');
+
+        return view('adminify::auth.passwords.reset')->with(
+            ['token' => $token, 'email' => $request->email]
+        );
+    }
+
 }
