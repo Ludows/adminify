be5429f6c92ad1f69e3144c842911e0558d77a73
l.cointrel <l.cointrel@mediactive.fr>
2021-06-25 14:52:13 +0200

    Tests for redirectTo

--- src/Http/Controllers/Auth/RegisterController.php
+++ src/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    // protected $redirectTo = route('home.dashboard');
+    protected $redirectTo = "/admin/dashboard";
 
     /**
      * Create a new controller instance.
