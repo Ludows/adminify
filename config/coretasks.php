f9c761c32b45e8a659f6daa4d7c7622c7ddaabd5
l.cointrel <l.cointrel@mediactive.fr>
2021-06-26 11:45:42 +0200

    Adding auth.php and routeserviceprovider

--- config/coretasks.php
+++ config/coretasks.php
@@ -1,7 +1,9 @@
 <?php
 
 return array(
+    __DIR__.'/../publishable/app/Providers/RouteServiceProvider.php' => app_path('Providers/RouteServiceProvider.php'),
     __DIR__.'/../config/check-permissions.php' => config_path('check-permissions.php'),
+    __DIR__.'/../config/auth.php' => config_path('auth.php'),
     __DIR__.'/../config/filesystems.php' => config_path('filesystems.php'),
     __DIR__.'/../config/site-settings.php' => config_path('site-settings.php'),
     __DIR__.'/../publishable/public/argon/' => public_path('argon'),
