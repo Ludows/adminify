9bc15591cf4a5f10e6def21aba52e155044d94f1
l.cointrel <l.cointrel@mediactive.fr>
2021-06-26 10:33:32 +0200

    Add filesystems config

--- src/AdminifyServiceProvider.php
+++ src/AdminifyServiceProvider.php
@@ -30,6 +30,7 @@ class AdminifyServiceProvider extends ServiceProvider {
         if($app->runningInConsole()) {
             $this->publishes(array(
                 __DIR__.'/../config/check-permissions.php' => config_path('check-permissions.php'),
+                __DIR__.'/../config/filesystems.php' => config_path('filesystems.php'),
                 __DIR__.'/../config/site-settings.php' => config_path('site-settings.php'),
                 __DIR__.'/../publishable/public/argon/' => public_path('argon'),
                 __DIR__.'/../publishable/webpack.mix.js' => base_path('webpack.mix.js'),
