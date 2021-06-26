e1d3220ae50e26369f1787654d35cbc4efe3fd52
l.cointrel <l.cointrel@mediactive.fr>
2021-06-26 11:55:43 +0200

    Adding lang resources for admin

--- config/coretasks.php
+++ config/coretasks.php
@@ -2,6 +2,7 @@
 
 return array(
     __DIR__.'/../publishable/app/Providers/RouteServiceProvider.php' => app_path('Providers/RouteServiceProvider.php'),
+    __DIR__.'/../publishable/resources/lang/' => resource_path('lang'),
     __DIR__.'/../config/check-permissions.php' => config_path('check-permissions.php'),
     __DIR__.'/../config/auth.php' => config_path('auth.php'),
     __DIR__.'/../config/filesystems.php' => config_path('filesystems.php'),
