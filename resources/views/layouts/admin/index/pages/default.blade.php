bea9846d4dccaba397c9e54098becc6fa7980b94
l.cointrel <l.cointrel@mediactive.fr>
2021-06-27 12:50:46 +0200

    If not datas

--- resources/views/layouts/admin/index/pages/default.blade.php
+++ resources/views/layouts/admin/index/pages/default.blade.php
@@ -44,27 +44,35 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($datas as $data)
+                                @if(count($datas) > 0)
+                                    @foreach($datas as $data)
+                                        <tr>
+                                            @foreach($thead as $th)
+                                                @php
+                                                    $slugBlade = Str::slug($th);
+                                                @endphp
+                                                @if(view()->exists('adminify::layouts.admin.index.blocks.'.$name.'-'.$slugBlade))
+                                                    @include('adminify::layouts.admin.index.blocks.'.$name.'-'.$slugBlade, ['data' => $data[$th] ,'model' => $data ])
+                                                @elseif(view()->exists('layouts.admin.index.blocks'.$slugBlade))
+                                                    @include('adminify::layouts.admin.index.blocks.'.$slugBlade, ['data' => $data[$th] ,'model' => $data])
+                                                @else
+                                                    <td>{{ $data[$th] }}</td>
+                                                @endif
+                                            @endforeach
+                                            <td>
+                                                @isset($actions)
+                                                    @include('adminify::layouts.admin.index.blocks.dropdown-actions', ['action' => $actions[$loop->index]])
+                                                @endisset
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
                                     <tr>
-                                        @foreach($thead as $th)
-                                            @php
-                                                $slugBlade = Str::slug($th);
-                                            @endphp
-                                            @if(view()->exists('adminify::layouts.admin.index.blocks.'.$name.'-'.$slugBlade))
-                                                @include('adminify::layouts.admin.index.blocks.'.$name.'-'.$slugBlade, ['data' => $data[$th] ,'model' => $data ])
-                                            @elseif(view()->exists('layouts.admin.index.blocks'.$slugBlade))
-                                                @include('adminify::layouts.admin.index.blocks.'.$slugBlade, ['data' => $data[$th] ,'model' => $data])
-                                            @else
-                                                <td>{{ $data[$th] }}</td>
-                                            @endif
-                                        @endforeach
                                         <td>
-                                            @isset($actions)
-                                                @include('adminify::layouts.admin.index.blocks.dropdown-actions', ['action' => $actions[$loop->index]])
-                                            @endisset
+                                            {{ _i('admin.noDatas') }}
                                         </td>
-                                    </tr>
-                                @endforeach
+                                    </tr>     
+                                @endif
                             </tbody>
                         </table>
                         <div class="mt-3">
