80de12fb96be32ce01089e96e791252561d2974e
l.cointrel <l.cointrel@mediactive.fr>
2021-06-27 23:57:30 +0200

    Fix js

--- resources/views/layouts/admin/index/blocks/paginate.blade.php
+++ resources/views/layouts/admin/index/blocks/paginate.blade.php
@@ -1,6 +1,6 @@
 <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
     <div class="btn-group" role="group" aria-label="First group">
-      <button type="button" disabled class="btn js-listing-btn btn-secondary">{{  _i('admin.previous') }}</button>
-      <button type="button" class="btn js-listing-btn btn-secondary">{{  _i('admin.next') }}</button>
+      <button type="button" data-direction="previous" class="btn js-listing-btn btn-primary">{{  _i('admin.previous') }}</button>
+      <button type="button" data-direction="next" class="btn js-listing-btn btn-primary">{{  _i('admin.next') }}</button>
     </div>
 </div>
