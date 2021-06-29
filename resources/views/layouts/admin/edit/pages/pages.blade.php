4a767c66869541d961608af3d4f1f04e72de622b
l.cointrel <l.cointrel@mediactive.fr>
2021-06-29 15:56:36 +0200

    Pages custom

---
+++ resources/views/layouts/admin/edit/pages/pages.blade.php
@@ -0,0 +1,22 @@
+<div class="container-fluid mt--7">
+    <div class="row">
+        <div class="col-12">
+            @php
+                $name = request()->route()->getName();
+                $name = str_replace('.edit', '', $name);
+            @endphp
+
+            <div class="card shadow bg-transparent">
+                {{-- <div class="card-header border-0">
+                    {{ __('edit.'.$name) }}
+                </div> --}}
+                <div class="card-body p-0">
+                    @if(isset($form))
+                        {!! form($form) !!}
+                    @endif
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
