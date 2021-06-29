101ece30b2fd5cc0b4a64fb6423d2ceca4191b69
l.cointrel <l.cointrel@mediactive.fr>
2021-06-29 17:04:02 +0200

    Fix call extend create

--- resources/views/layouts/admin/pages/create.blade.php
+++ resources/views/layouts/admin/pages/create.blade.php
@@ -5,7 +5,7 @@
         $name = request()->route()->getName();
         $name = str_replace('.create', '', $name);
     @endphp
-    @if(view()->exists('layouts.admin.create.pages.'.$name))
+    @if(view()->exists('adminify::layouts.admin.create.pages.'.$name))
         @include('adminify::layouts.admin.create.pages.'.$name)
     @else
         @include('adminify::layouts.admin.create.pages.default')
