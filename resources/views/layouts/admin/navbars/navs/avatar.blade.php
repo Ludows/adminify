d06ba7d66d9f891dc5f163bdcad4470a32b911cd
l.cointrel <l.cointrel@mediactive.fr>
2021-06-30 17:36:15 +0200

    Ajusts dropdown user

--- resources/views/layouts/admin/navbars/navs/avatar.blade.php
+++ resources/views/layouts/admin/navbars/navs/avatar.blade.php
@@ -1,20 +1,15 @@
 
 @php
     $withName = isset($withName) ? $withName : false;
+    $user = auth()->user();
+    $avatar = $user->avatar;
 @endphp
 <div class="media align-items-center">
-    <span class="avatar avatar-sm rounded-circle">
-        @php
-            $user = auth()->user();
-            $avatar = $user->avatar;
-        @endphp
-        @if(isset($avatar))
-            <img alt="Image placeholder" src="{{ asset('myuploads/medias/') }}/{{ $avatar->src }}">
-        @else
-            @todo
-            {{-- <img alt="Image placeholder" src="{{ asset('argon') }}/img/theme/team-1-800x800.jpg"> --}}
-        @endif
-    </span>
+    @if(isset($avatar))
+        <span class="avatar avatar-sm rounded-circle">
+            <img alt="{{ $user->name }}" src="{{ $avatar->path }}">
+        </span>
+    @endif
     @if($withName)
         <div class="media-body ml-2 d-none d-lg-block">
             <span class="mb-0 text-sm  font-weight-bold">{{ $user->name }}</span>
