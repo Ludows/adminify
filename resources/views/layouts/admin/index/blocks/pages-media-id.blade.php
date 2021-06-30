28bb325e39e31910e6766b3432d8b03ce5848760
l.cointrel <l.cointrel@mediactive.fr>
2021-06-30 15:11:57 +0200

    Pages media id

--- resources/views/layouts/admin/index/blocks/pages-media-id.blade.php
+++ resources/views/layouts/admin/index/blocks/pages-media-id.blade.php
@@ -1,5 +1,5 @@
 {{-- {{ dd($data) }} --}}
-@if(gettype($data) == 'integer')
+@if(gettype($data) == 'integer' && $data != 0)
     <td><img class="img-fluid" alt="{{ $model->media->src  }}" src="{{ $model->media->path }}" ></td>
 @else
     <td> {{ _i('admin.hasNoImage') }}</td>
