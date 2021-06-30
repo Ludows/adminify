ec77e1e3cb749f063c1b5b843f1f928fe84d4778
l.cointrel <l.cointrel@mediactive.fr>
2021-06-30 11:55:22 +0200

    WIP dropdowns

--- src/Dropdowns/Translations.php
+++ src/Dropdowns/Translations.php
@@ -1,51 +1,58 @@
 <?php
 namespace Ludows\Adminify\Dropdowns;
 
-use Ludows\Adminify\Libs\Dropdowns;
-
-class Translations extends Dropdowns
+use Ludows\Adminify\Libs\DropdownsManager;
+use Ludows\Adminify\Forms\DeleteCrud;
+class Translations extends DropdownsManager
 {
     public function getView() {
         //you can overwrite parent view
         $r = $this->getRequest();
-        return $r->exists('missing_translations') ? 'adminify::layouts.admin.actionable.missing-translations' : parent::getView();
+        return $r->exists('missing_translations') ? 'adminify::layouts.admin.dropdowns.missing-translations' : parent::getView();
     }
     public function handle() {
 
         $datas = $this->getDatas();
         $r = $this->getRequest();
-        $m = $this->getModels();
+        $models = $this->getModels();
 
-        if($m->isMultiLangModel()) {
-            $missing = $m->getNeededTranslations();
-            if(count($missing) > 0 && $r->exists('missing_translations')) {
-                foreach ($missing as $miss) {
-                    # code...
-                    $this->add('edit_'.$miss, [
-                        'template' =>  'adminify::actions.edit',
-                        'vars' => [
-                            'url' => route('traductions.edit', ['traduction' => $m->id, 'lang' => $r->useMultilang ? $miss : '']),
-                            'name' => 'traductions'
-                        ]
-                    ]);
+        $form = app('Kris\LaravelFormBuilder\FormBuilder');
+
+        foreach ($models as $m) {
+            if($m->isMultiLangModel()) {
+                $missing = $m->getNeededTranslations();
+                if(count($missing) > 0 && $r->exists('missing_translations')) {
+                    foreach ($missing as $miss) {
+                        # code...
+                        $this->add('dropdown_'.$m->id, [
+                            'template' =>  'adminify::layouts.admin.dropdowns.extends.edit',
+                            'vars' => [
+                                'url' => route('traductions.edit', ['traduction' => $m->id, 'lang' => $r->useMultilang ? $miss : '']),
+                                'name' => 'traductions'
+                            ]
+                        ]);
+                    }
                 }
             }
+            if(!$r->exists('missing_translations')) {
+                $this->add('dropdown_'.$m->id, [
+                    'template' => 'adminify::layouts.admin.dropdowns.extends.edit',
+                    'vars' => [
+                        'url' => route('traductions.edit', ['traduction' => $m->id, 'lang' => $r->useMultilang ? $miss : '']),
+                        'name' => 'traductions'
+                    ]
+                ]);
+                $this->add('dropdown_'.$m->id, [
+                    'template' => 'adminify::layouts.admin.dropdowns.extends.delete',
+                    'vars' => [
+                        'form' => $form->create(DeleteCrud::class, [
+                            'method' => 'DELETE',
+                            'url' => route('traductions.destroy', ['traduction' => $m->id])
+                        ])
+                    ]
+                ]);
+            }
         }
-        if(!$r->exists('missing_translations')) {
-            $this->add('edit', [
-                'template' => 'adminify::actions.edit',
-                'vars' => [
-                    'editParam' => $datas['singular'],
-                    'id' => $datas['pageId'],
-                    'name' => 'traductions'
-                ]
-            ]);
-            $this->add('delete', [
-                'template' => 'adminify::actions.delete',
-                'vars' => [
-                    'form' => $datas['form'],
-                ]
-            ]);
-        }
+        
     }
 }
