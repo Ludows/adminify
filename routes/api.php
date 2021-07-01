b8417c7efe9dca020f1fb6045b7829c4221cca9c
l.cointrel <l.cointrel@mediactive.fr>
2021-07-01 14:48:43 +0200

    Tests

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-$config = config('site-settings.restApi');
+// $config = config('site-settings.restApi');
 
 // if($config['enable']) {
 //     //$config crud
