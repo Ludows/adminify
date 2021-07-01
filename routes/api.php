7ea7c261403610b218219855d104b6765c2bd272
l.cointrel <l.cointrel@mediactive.fr>
2021-07-01 15:23:28 +0200

    Restore routes api

--- routes/api.php
+++ routes/api.php
@@ -2,24 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-// $config = config('site-settings.restApi');
+$config = config('site-settings.restApi');
 
-// if($config['enable']) {
-//     //$config crud
-//     if(count($config['crud']) > 0) {
-//         foreach ($config['crud'] as $key => $classValue) {
-//             # code...
-//             Route::resource($key, $classValue);
+if($config['enable']) {
+    //$config crud
+    if(count($config['crud']) > 0) {
+        foreach ($config['crud'] as $key => $classValue) {
+            # code...
+            Route::resource($key, $classValue);
     
-//         }
-//     }
-//     if(count($config['customRoutes']) > 0) {
-//         foreach ($config['customRoutes'] as $arrayValues) {
-//             # code...
-//             foreach ($arrayValues['verbs'] as $verb) {
-//                 # code...
-//                 Route::{$verb}($arrayValues['route'], $arrayValues['controller']);
-//             }
-//         }
-//     }
-// }
+        }
+    }
+    if(count($config['customRoutes']) > 0) {
+        foreach ($config['customRoutes'] as $arrayValues) {
+            # code...
+            foreach ($arrayValues['verbs'] as $verb) {
+                # code...
+                Route::{$verb}($arrayValues['route'], $arrayValues['controller']);
+            }
+        }
+    }
+}
