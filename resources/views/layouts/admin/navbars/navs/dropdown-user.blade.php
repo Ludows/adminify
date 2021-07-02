0f11aeb9ca8b3e1c6f0fea1ce62624ba8356182b
l.cointrel <l.cointrel@mediactive.fr>
2021-07-02 11:00:59 +0200

    Change translation for dropdown user

--- resources/views/layouts/admin/navbars/navs/dropdown-user.blade.php
+++ resources/views/layouts/admin/navbars/navs/dropdown-user.blade.php
@@ -3,24 +3,24 @@
 @endphp
 <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
     <div class=" dropdown-header noti-title">
-        <h6 class="text-overflow m-0">{{ __('Welcome!') }}</h6>
+        <h6 class="text-overflow m-0">{{ __('admin.welcome') }}</h6>
     </div>
     @if($user->hasPermissionTo('edit_users'))
         <a href="{{ route('users.edit', ['user' => $user->id]) }}" class="dropdown-item">
             <i class="ni ni-single-02"></i>
-            <span>{{ __('My profile') }}</span>
+            <span>{{ __('admin.profile') }}</span>
         </a>
     @endif
     @if($user->hasPermissionTo('manage_settings'))
         <a href="{{ route('settings.index') }}" class="dropdown-item">
             <i class="ni ni-settings-gear-65"></i>
-            <span>{{ __('Settings') }}</span>
+            <span>{{ __('admin.settings.index') }}</span>
         </a>
     @endif
     <div class="dropdown-divider"></div>
     <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
     document.getElementById('logout-form').submit();">
         <i class="ni ni-user-run"></i>
-        <span>{{ __('Logout') }}</span>
+        <span>{{ __('admin.logout') }}</span>
     </a>
 </div>
