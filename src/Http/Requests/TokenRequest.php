a1d64938a88326149985e3f8b39446092e429fd2
l.cointrel <l.cointrel@mediactive.fr>
2021-07-02 08:55:34 +0200

    Token Request

---
+++ src/Http/Requests/TokenRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ludows\Adminify\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class TokenRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $r = request();
+        $a = [
+            'name' => [
+                'required', 'min:3'
+            ],
+            'email' => [
+                'required', 'email', Rule::unique((new User)->getTable())->ignore($this->route()->user->id ?? null)
+            ],
+            'password' => [
+                $this->route()->user ? 'nullable' : 'required', 'confirmed', 'min:6'
+            ]
+        ];
+
+        if($r->is('api.*')) {
+            $a['token'] = [
+                'required'
+            ];
+        }
+
+        if($r->useMultilang && $r->is('api.*')) {
+            $a['lang'] = [
+                'required'
+            ];
+        }
+
+        return $a;
+    }
+}
