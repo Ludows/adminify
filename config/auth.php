7cb416f362c8fb49ae0bc8394b4455b0bb716444
l.cointrel <l.cointrel@mediactive.fr>
2021-07-03 14:02:13 +0200

    Changes auth

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => \Ludows\Adminify\Models\User::class,
+            'model' => \App\Models\User::class,
         ],
 
         // 'users' => [
