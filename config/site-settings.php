152ca60e407be8eb0704bdeda5ce711763ad9ef0
l.cointrel <l.cointrel@mediactive.fr>
2021-07-04 17:01:05 +0200

    Adding requests stubs

--- config/site-settings.php
+++ config/site-settings.php
@@ -8,10 +8,10 @@ return [
     // menu builder is automatic sync with multilang param.
     'menu-builder' => [
         'models' => [
-            'category' => Ludows\Adminify\Models\Category::class,
-            'page' => Ludows\Adminify\Models\Page::class,
-            'post' => Ludows\Adminify\Models\Post::class,
-            'custom' => Ludows\Adminify\Models\CustomLink::class
+            'category' => App\Models\Category::class,
+            'page' => App\Models\Page::class,
+            'post' => App\Models\Post::class,
+            'custom' => App\Models\CustomLink::class
         ],
         'showAlways' => [
             'custom' => [ // you can pass some field to build your form
@@ -31,11 +31,11 @@ return [
     'searchable' => [
         'admin' => [
             'models' => [
-                'posts' => \Ludows\Adminify\Models\Post::class,
-                'users' => \Ludows\Adminify\Models\User::class,
-                'menus' => \Ludows\Adminify\Models\Menu::class,
-                'pages' => \Ludows\Adminify\Models\Page::class,
-                'traductions' => \Ludows\Adminify\Models\Translations::class
+                'posts' => \App\Models\Post::class,
+                'users' => \App\Models\User::class,
+                'menus' => \App\Models\Menu::class,
+                'pages' => \App\Models\Page::class,
+                'traductions' => \App\Models\Translations::class
             ],
             'labels' => [
                 'posts' => 'title',
@@ -67,37 +67,37 @@ return [
         'search' => [
             'post' => [
                 'dropdownManager' => \Ludows\Adminify\Dropdowns\Post::class,
-                'class' => \Ludows\Adminify\Models\Post::class,
+                'class' => \App\Models\Post::class,
                 'columns' => []
             ],
             'user' => [
                 'dropdownManager' => \Ludows\Adminify\Dropdowns\Users::class,
-                'class' => \Ludows\Adminify\Models\User::class,
+                'class' => \App\Models\User::class,
                 'columns' => []
             ],
             'menu' => [
                 'dropdownManager' => \Ludows\Adminify\Dropdowns\Menu::class,
-                'class' => \Ludows\Adminify\Models\Menu::class,
+                'class' => \App\Models\Menu::class,
                 'columns' => []
             ],
             'page' => [
                 'dropdownManager' => \Ludows\Adminify\Dropdowns\Page::class,
-                'class' => \Ludows\Adminify\Models\Page::class,
+                'class' => \App\Models\Page::class,
                 'columns' => []
             ],
             'category' => [
                 'dropdownManager' => \Ludows\Adminify\Dropdowns\Category::class,
-                'class' => \Ludows\Adminify\Models\Category::class,
+                'class' => \App\Models\Category::class,
                 'columns' => []
             ],
             'comment' => [
                 'dropdownManager' => \Ludows\Adminify\Dropdowns\Comment::class,
-                'class' => \Ludows\Adminify\Models\Comment::class,
+                'class' => \App\Models\Comment::class,
                 'columns' => []
             ],
             'traduction' => [
                 'dropdownManager' => \Ludows\Adminify\Dropdowns\Translations::class,
-                'class' => \Ludows\Adminify\Models\Translations::class,
+                'class' => \App\Models\Translations::class,
                 'columns' => []
             ],
         ]
@@ -139,38 +139,38 @@ return [
         'limit' => 3,
         'blocks' => [
             'page' => [
-                'class' => Ludows\Adminify\Models\Page::class,
+                'class' => \App\Models\Page::class,
                 'showIf' => ['administrator', 'client'],
                 'template' => 'adminify::layouts.admin.dashboard.card-page',
                 'columnShow' => 'title',
             ],
             'post' => [
-                'class' => Ludows\Adminify\Models\Post::class,
+                'class' => \App\Models\Post::class,
                 'showIf' => ['administrator', 'client'],
                 'template' => 'adminify::layouts.admin.dashboard.card-post',
                 'columnShow' => 'title',
             ],
             'media' => [
-                'class' => Ludows\Adminify\Models\Media::class,
+                'class' => \App\Models\Media::class,
                 'showIf' => ['administrator', 'client'],
                 'template' => 'adminify::layouts.admin.dashboard.card-media',
                 'columnShow' => 'title',
                 'plural' => 'medias'
             ],
             'category' => [
-                'class' => Ludows\Adminify\Models\Category::class,
+                'class' => \App\Models\Category::class,
                 'showIf' => ['administrator', 'client'],
                 'template' => 'adminify::layouts.admin.dashboard.card-category',
                 'columnShow' => 'title',
             ],
             'traduction' => [
-                'class' => Ludows\Adminify\Models\Translations::class,
+                'class' => \App\Models\Translations::class,
                 'showIf' => ['administrator', 'client'],
                 'template' => 'adminify::layouts.admin.dashboard.card-traduction',
                 'columnShow' => 'key',
             ],
             'comment' => [
-                'class' => Ludows\Adminify\Models\Comment::class,
+                'class' => \App\Models\Comment::class,
                 'showIf' => ['administrator', 'client'],
                 'template' => 'adminify::layouts.admin.dashboard.card-comment',
                 'columnShow' => 'comment',
