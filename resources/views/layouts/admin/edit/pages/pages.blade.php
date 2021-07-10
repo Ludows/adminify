71b15a4d4be626b1f20b4e725e117419e93c9393
l.cointrel <l.cointrel@mediactive.fr>
2021-07-10 16:27:43 +0200

    Update

--- resources/views/layouts/admin/edit/pages/pages.blade.php
+++ resources/views/layouts/admin/edit/pages/pages.blade.php
@@ -1,7 +1,7 @@
 @include('adminify::layouts.admin.headers.topPageListing')
 
 <div class="container-fluid editor mt--7">
-    <div class="row">
+    <div class="row no-gutters">
         <div class="col-12">
             @php
                 $name = request()->route()->getName();
@@ -12,7 +12,7 @@
                 {{-- <div class="card-header border-0">
                     {{ __('edit.'.$name) }}
                 </div> --}}
-                <div class="card-body p-0">
+                <div class="card-body">
                     @if(isset($form))
                         {!! form($form) !!}
                     @endif
