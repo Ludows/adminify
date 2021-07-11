71e6f762fc46237a294ced5dc8c79632f1ffe0d5
l.cointrel <l.cointrel@mediactive.fr>
2021-07-11 14:58:36 +0200

    Test

--- resources/views/layouts/admin/table/custom-cells/posts-media-id.blade.php
+++ resources/views/layouts/admin/table/custom-cells/posts-media-id.blade.php
@@ -1,6 +1,6 @@
 {{-- {{ dd($data) }} --}}
-@if(gettype($data) == 'integer' && $data != 0)
-    <td><img class="img-fluid" alt="{{ $model->media->src  }}" src="{{ asset('myuploads/medias/') }}/{{ $model->media->src }}" ></td>
+@if(gettype($model->{$attr}) == 'integer' && $model->{$attr} != 0)
+    <td><img class="img-fluid" alt="{{ $model->media->src  }}" src="{{ $model->media->path ?? '' }}" ></td>
 @else
     <td> {{ __('admin.hasNoImage') }} </td>
 @endif
