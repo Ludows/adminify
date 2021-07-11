a0846d6aff94f43aab35f0eab41e1869546c4d4d
l.cointrel <l.cointrel@mediactive.fr>
2021-07-11 17:35:52 +0200

    Adding full translated mode

--- resources/views/layouts/admin/table/custom-cells/translated.blade.php
+++ resources/views/layouts/admin/table/custom-cells/translated.blade.php
@@ -1,7 +1,12 @@
 <td>
-    @foreach ($routes as $route)
-        <a href="{{ $route }}" class="btn btn-warning btn-lg">
-            {{ __('admin.translate.'.$missing[$loop->index]) }}
-        </a>
-    @endforeach
+    @if(count($routes) > 0)
+        @foreach ($routes as $route)
+            <a href="{{ $route }}" class="btn btn-warning btn-lg">
+                {{ __('admin.translate.'.$missing[$loop->index]) }}
+            </a>
+        @endforeach
+    @else
+        {{ __('admin.is_translated') }}
+    @endif
+    
 </td>
