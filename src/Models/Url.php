4ca97fb771d738ebf88f21262a9f81b3a712714f
l.cointrel <l.cointrel@mediactive.fr>
2021-07-15 08:50:14 +0200

    Url Fix model

--- src/Models/Url.php
+++ src/Models/Url.php
@@ -5,11 +5,16 @@ namespace Ludows\Adminify\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Spatie\Translatable\HasTranslations;
+use Ludows\Adminify\Traits\MultilangTranslatableSwitch;
+
 use Ludows\Adminify\Traits\Helpers;
 class Url extends Model
 {
     protected $table = 'urls';
     use HasFactory;
+    use HasTranslations;
+    use MultilangTranslatableSwitch;
     use Helpers;
     
     public $MultilangTranslatableSwitch = ['data'];
