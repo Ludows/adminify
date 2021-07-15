56e1df480a632fc2407f1d22c44b8141a9fa7c26
l.cointrel <l.cointrel@mediactive.fr>
2021-07-15 09:06:31 +0200

    Adding Url trait and attribute

--- src/Traits/Urlable.php
+++ src/Traits/Urlable.php
@@ -2,11 +2,26 @@
 
   namespace Ludows\Adminify\Traits;
 
-  use Ludows\Adminify\Models\Url;
+  use App\Models\Url;
   trait Urlable
   {
-   //
-   public function url() {
-        dd($this);
-   }
+     //
+     public function url($model = null) {
+          // define as getter and setter
+          $u = new Url();
+          if(is_null($model)) {
+               $u->model_name = $model->getNameSpace();
+               $u->model_id = $model->id;
+               $u->data = $model->title;
+               $u->save();
+          }
+          else {
+               $u->where('model_id', $this->id);
+          }
+
+          return $u;
+     }
+     public function urlAttribute() {
+          return $this->url();
+     }
   }
