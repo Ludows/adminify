5d476d8d48570ba6f30fbf15d2e6feccf78d2e1b
l.cointrel <l.cointrel@mediactive.fr>
2021-07-15 08:40:05 +0200

    Add migration and model url

---
+++ database/migrations/2021_10_12_000001_create_urls_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('urls');
+        
+        if(config('site-settings.multilang') == true) {
+            Schema::create('urls', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('model_name', 255);
+                $table->bigInteger('model_id');
+                $table->json('data');
+                $table->timestamps();
+            });
+        }
+        else {
+            Schema::create('urls', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('model_name', 255);
+                $table->bigInteger('model_id');
+                $table->string('data', 255);
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
---
+++ src/Models/Url.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ludows\Adminify\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Ludows\Adminify\Traits\Helpers;
+class Url extends Model
+{
+    protected $table = 'urls';
+    use HasFactory;
+    use Helpers;
+    
+    public $MultilangTranslatableSwitch = ['data'];
+
+    protected $fillable = [
+        'model_name',
+        'model_id', //the path you uploaded the image
+        'data',
+    ];
+}
