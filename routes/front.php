bd91ec9a91d559b9b16b3ebc535c321af19969fc
l.cointrel <l.cointrel@mediactive.fr>
2021-07-19 00:02:41 +0200

    Changing name front pagess

--- routes/front.php
+++ routes/front.php
@@ -6,7 +6,7 @@ $c = config('site-settings.restApi');
 if(request()->segment(1) != $c['prefix']) {
     Route::middleware('multilang.basic')->group(function ($router) {
         $router->get('/', 'Ludows\Adminify\Http\Controllers\Front\PageController@index')->name('pages.front.index');
-        $router->get('{slug}', 'Ludows\Adminify\Http\Controllers\Front\PageController@getPages')->where('slug', '([A-Za-z0-9\-\/]+)')->name('pages.front.show')->middleware('front.blogpage');
+        $router->get('{slug}', 'Ludows\Adminify\Http\Controllers\Front\PageController@getPages')->where('slug', '([A-Za-z0-9\-\/]+)')->name('pages.front.show')->middleware('pages.front.pages');
         $router->bind('slug', 'Ludows\Adminify\Http\Controllers\Front\PageController@handleSlug');
     });
 }
