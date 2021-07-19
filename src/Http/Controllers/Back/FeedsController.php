1e74ddbe07afc5bf01d164d830d231452afd12f5
l.cointrel <l.cointrel@mediactive.fr>
2021-07-19 17:29:56 +0200

    Adding feeds and sitemaps base workflow

---
+++ src/Http/Controllers/Back/FeedsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ludows\Adminify\Http\Controllers\Back;
+
+use Illuminate\Http\Request;
+use Ludows\Adminify\Http\Controllers\Controller;
+
+class FeedsController extends Controller
+{
+    public function index(Request $request) {
+
+    }
+}
---
+++ src/Http/Controllers/Back/SitemapController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ludows\Adminify\Http\Controllers\Back;
+
+use Illuminate\Http\Request;
+use Ludows\Adminify\Http\Controllers\Controller;
+
+class SitemapController extends Controller
+{
+    public function index(Request $request) {
+
+    }
+}
