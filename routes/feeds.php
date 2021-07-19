46243ed5dc95e5ef3eaad481fd218f3781422f18
l.cointrel <l.cointrel@mediactive.fr>
2021-07-19 17:38:07 +0200

    Adding fixes to routes

--- routes/feeds.php
+++ routes/feeds.php
@@ -1,6 +1,4 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::group(function ($router) {
-    $router->get('/feeds/{slug}', 'Ludows\Adminify\Http\Controllers\Back\FeedsController@index')->name('feed.index');
-});
+Route::get('/feeds/{slug?}', 'Ludows\Adminify\Http\Controllers\Back\FeedsController@index')->name('feed.index');
--- routes/sitemaps.php
+++ routes/sitemaps.php
@@ -1,6 +1,4 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::group(function ($router) {
-    $router->get('/sitemap/{slug}', 'Ludows\Adminify\Http\Controllers\Back\SitemapController@index')->name('sitemap.index');
-});
+Route::get('/sitemap/{slug?}', 'Ludows\Adminify\Http\Controllers\Back\SitemapController@index')->name('sitemap.index');
