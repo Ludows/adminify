9bb851bd4b876aa7feb0bd775786c8edc1e0d0a8
l.cointrel <l.cointrel@mediactive.fr>
2021-07-19 09:36:10 +0200

    Save html from summernote

--- publishable/resources/adminify/js/extensions/summernoteField.js
+++ publishable/resources/adminify/js/extensions/summernoteField.js
@@ -4,8 +4,15 @@ export default function summernoteInititalization(fields) {
         let hiddenField = $('#'+el.selector+' input[type="hidden"]');
         let summernote = textField.summernote(el.options);
 
+        let form = hiddenField.closest("form");
+
         if(hiddenField.val().length > 0) {
             textField.summernote('code', hiddenField.val());
         }
+
+        form.on('submit', function(e) {
+            let code = textField.summernote('code');
+            hiddenField.val(code);
+        })
     })
 }
