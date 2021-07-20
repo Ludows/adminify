033eb68b14b8e45511194052ab40aff13d0ecb1d
l.cointrel <l.cointrel@mediactive.fr>
2021-07-20 17:48:51 +0200

    Fix feeds

--- config/feed.php
+++ config/feed.php
@@ -1,8 +1,3 @@
 <?php
 
-$classical_config = get_config_feeds();
-// if you want to had more feeds. Just merge you array config to get_config_feeds()
-
-return [
-    'feeds' => $classical_config 
-];
+return [];
