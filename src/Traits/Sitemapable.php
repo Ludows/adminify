94e471c9be5c37d499404ab7759d343070e4f010
l.cointrel <l.cointrel@mediactive.fr>
2021-07-20 23:19:28 +0200

    Debug

--- src/Traits/Sitemapable.php
+++ src/Traits/Sitemapable.php
@@ -5,6 +5,16 @@
   trait Sitemapable
   {
      // tell which column to show for your url part
-     public $sitemapTitle = 'title';
-     public $sitemapCallable = null;
+   //   public $sitemapTitle = 'title';
+   //   public $sitemapCallable = null;
+     public function getsitemapTitleProperty() {
+         return isset($this->sitemapTitle) ?
+         $this->sitemapTitle:
+         'title';
+     }
+     public function getsitemapCallableProperty() {
+      return isset($this->sitemapCallable) ?
+      $this->sitemapCallable:
+      null;
+  }
   }
