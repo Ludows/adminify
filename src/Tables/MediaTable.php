e20136826c19909a9f307ac7ad1d3c2859314aaa
l.cointrel <l.cointrel@mediactive.fr>
2021-07-21 11:49:49 +0200

    Update Media table

--- src/Tables/MediaTable.php
+++ src/Tables/MediaTable.php
@@ -25,18 +25,30 @@ class MediaTable extends TableManager {
         $model = new MediaModel();
         $fillables = $model->getFillable();
         $datas = $this->getDatas();
+        $request = $this->getRequest();
         
         if(isset($datas['results'])) {
             $medias = $datas['results'];
         }
         else {
-            $medias = MediaModel::limit( $config['limit'] )->get();
+            if($request->useMultilang) {
+                $medias = MediaModel::limit( $config['limit'] )->lang($request->lang)->get();
+                // dd($categories);
+            }
+            else {
+                $medias = MediaModel::limit( $config['limit'] )->get();;
+            }
         }
         
 
         $a = new MediaDropdownsManager($medias, []);
         // set columns
-        $this->columns( array_merge($fillables, ['actions']) );
+        $default_merge_columns = ['actions'];
+
+        if($request->useMultilang && is_translatable_model($model)) {
+            array_unshift($default_merge_columns, 'need_translations');
+        }
+        $this->columns( array_merge($fillables, $default_merge_columns) );
 
 
         foreach ($medias as $media) {
@@ -47,6 +59,14 @@ class MediaTable extends TableManager {
                 # code...
                 $table->column($fillable, $this->getTemplateByName($fillable));
             }
+            
+            if($request->useMultilang && is_translatable_model($model)) {
+                $table->column('need_translations', 'adminify::layouts.admin.table.custom-cells.translated', [
+                    'routes' => get_missing_translations_routes('savetraductions.edit', 'savetraduction', $this->getModel()),
+                    'missing' => get_missing_langs($this->getModel()),
+                ]);
+            }
+
             $table->column('actions', 'adminify::layouts.admin.table.custom-cells.dropdown', [
                 'dropdown' => $a,
                 'index' => $media->id
