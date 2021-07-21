17eb34727b2b11f0d84fbdcc5a13a315e43b96c5
l.cointrel <l.cointrel@mediactive.fr>
2021-07-21 09:21:18 +0200

    Adding Basic Model and ContentTypeModel

--- src/Models/ClassicModel.php
+++ src/Models/ClassicModel.php
@@ -7,23 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
 use Ludows\Adminify\Traits\OnBootedModel;
-use Ludows\Adminify\Traits\Urlable;
 use Spatie\Translatable\HasTranslations;
 use Ludows\Adminify\Traits\MultilangTranslatableSwitch;
 use Ludows\Adminify\Traits\Helpers;
-use Ludows\Adminify\Traits\Sitemapable;
-
-use Spatie\Searchable\Searchable;
-use Spatie\Searchable\SearchResult;
-
-
 class BaseModel extends Model
 {
     use HasFactory;
     use OnBootedModel;
     // use Urlable;
-    // use HasTranslations;
-    // use MultilangTranslatableSwitch;
+    use HasTranslations;
+    use MultilangTranslatableSwitch;
     // use Sitemapable;
     use Helpers;
 }
--- src/Models/ContentTypeModel.php
+++ src/Models/ContentTypeModel.php
@@ -6,24 +6,39 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
-use Ludows\Adminify\Traits\OnBootedModel;
 use Ludows\Adminify\Traits\Urlable;
-use Spatie\Translatable\HasTranslations;
-use Ludows\Adminify\Traits\MultilangTranslatableSwitch;
 use Ludows\Adminify\Traits\Helpers;
 use Ludows\Adminify\Traits\Sitemapable;
 
 use Spatie\Searchable\Searchable;
 use Spatie\Searchable\SearchResult;
 
+use Spatie\Feed\Feedable;
+use Spatie\Feed\FeedItem;
+use Ludows\Adminify\Traits\HasSeo;
 
-class BaseModel extends Model
+use App\Models\ClassicModel; 
+
+class ContentTypeModel extends ClassicModel implements Searchable, Feedable
 {
     use HasFactory;
-    use OnBootedModel;
-    // use Urlable;
-    // use HasTranslations;
-    // use MultilangTranslatableSwitch;
-    // use Sitemapable;
+    use Urlable;
+    use HasSeo;
+    use Sitemapable;
     use Helpers;
+
+    public function getSearchResult(): SearchResult
+    {}
+
+    public function toFeedItem(): FeedItem
+    {
+        return FeedItem::create([
+            'id' => $this->id,
+            'title' => $this->title,
+            'summary' => $this->seoWith('description') ?? '',
+            'updated' => $this->updated_at,
+            'link' => $this->urlpath,
+            'authorName' => $this->author->name,
+        ]);
+    }
 }
