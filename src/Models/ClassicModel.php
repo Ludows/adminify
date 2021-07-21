b8bf5478209d42be592115648774a514c216e454
l.cointrel <l.cointrel@mediactive.fr>
2021-07-21 13:29:41 +0200

    Adding classic method

--- src/Models/ClassicModel.php
+++ src/Models/ClassicModel.php
@@ -10,6 +10,7 @@ use Ludows\Adminify\Traits\OnBootedModel;
 use Spatie\Translatable\HasTranslations;
 use Ludows\Adminify\Traits\MultilangTranslatableSwitch;
 use Ludows\Adminify\Traits\Helpers;
+use Ludows\Adminify\Traits\Formidable;
 
 use Spatie\Feed\Feedable;
 
@@ -26,4 +27,5 @@ abstract class ClassicModel extends Model implements Searchable, Feedable
     use MultilangTranslatableSwitch;
     // use Sitemapable;
     use Helpers;
+    use Formidable;
 }
