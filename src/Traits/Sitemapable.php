21a28bac3baab2d331b6df76886103c36e7ee0c1
l.cointrel <l.cointrel@mediactive.fr>
2021-07-23 15:30:06 +0200

    Allow or not generation of the sitemap when required for content type model

--- src/Traits/Sitemapable.php
+++ src/Traits/Sitemapable.php
@@ -5,6 +5,7 @@
   trait Sitemapable
   {
      // tell which column to show for your url part
+     public $allowSitemap = true;
      public $sitemapTitle = 'title';
      public $sitemapCallable = null;
   }
