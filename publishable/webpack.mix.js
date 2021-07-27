96c80ccce02c73cf60875e075b6996bf2144d4bf
l.cointrel <l.cointrel@mediactive.fr>
2021-07-27 10:56:34 +0200

    Adding missing line

--- publishable/webpack.mix.js
+++ publishable/webpack.mix.js
@@ -18,6 +18,7 @@ mix.js('resources/adminify/js/argon.js', 'public/argon/js')
     .js('resources/adminify/js/extensions-call.js', 'public/argon/js')
     .js('resources/adminify/js/searchable.js', 'public/argon/js')
     .js('resources/adminify/js/listings.js', 'public/argon/js')
+    .js('resources/adminify/js/menuBuilder.js', 'public/argon/js')
     .vue('resources/adminify/js/app.js', 'public/argon/js')
     .sass('resources/adminify/sass/argon.scss', 'public/argon/css')
     .sass('resources/adminify/sass/extensions.scss', 'public/argon/css');
