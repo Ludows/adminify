25bcd46b4536da576c291fd42c250b7c2ce4d206
l.cointrel <l.cointrel@mediactive.fr>
2021-07-27 17:43:21 +0200

    Fullmode on login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
-@extends('adminify::layouts.admin.app', ['class' => 'bg-default'])
+@extends('adminify::layouts.admin.app', ['class' => 'bg-default', 'fullmode' => true])
 
 @section('content')
-    {{-- @include('adminify::layouts.admin.headers.guest') --}}
+    @include('adminify::layouts.admin.headers.guest')
 
     <div class="container mt--8 pb-5">
         <div class="row justify-content-center">
