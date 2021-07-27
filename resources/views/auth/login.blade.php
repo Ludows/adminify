1c1b5ccabc02f7023323da46d0cf647cbc235e44
l.cointrel <l.cointrel@mediactive.fr>
2021-07-27 17:50:05 +0200

    AJustements admins

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,8 +7,8 @@
         <div class="row justify-content-center">
             <div class="col-lg-5 col-md-7">
                 <div class="card bg-secondary shadow border-0">
-                    <div class="card-header bg-transparent pb-5">
-                        <div class="text-muted text-center mt-2 mb-3"><small>{{ __('Sign in with') }}</small></div>
+                    <div class="card-header bg-transparent pb-0 border-bottom-0">
+                        <div class="text-muted text-center mt-2 mb-0"><small>{{ __('Sign in with') }}</small></div>
                         {{-- <div class="btn-wrapper text-center">
                             <a href="#" class="btn btn-neutral btn-icon">
                                 <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/github.svg"></span>
@@ -21,13 +21,16 @@
                         </div> --}}
                     </div>
                     <div class="card-body px-lg-5 py-lg-5">
-                        <div class="text-center text-muted mb-4">
-                            <small>
-                                    Create new account OR Sign in with these credentials:
-                                    <br>
-                                    Username <strong>admin@argon.com</strong> Password: <strong>secret</strong>
-                            </small>
-                        </div>
+                        @if (env('APP_ENV') == 'local')
+                            <div class="text-center text-muted mb-4">
+                                <small>
+                                        Create new account OR Sign in with these credentials:
+                                        <br>
+                                        Username <strong>admin@argon.com</strong> Password: <strong>secret</strong>
+                                </small>
+                            </div>
+                        @endif
+                        
                         <form role="form" method="POST" action="{{ route('login') }}">
                             @csrf
 
