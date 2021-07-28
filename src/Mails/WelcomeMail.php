48ad6a973b2ab616bb7b69d8c436b06c286d70b8
l.cointrel <l.cointrel@mediactive.fr>
2021-07-28 21:03:04 +0200

    Create Classic Mail

---
+++ src/Mails/WelcomeMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ludows\Adminify\Mails;
+
+use Spatie\MailTemplates\TemplateMailable;
+
+class WelcomeMail extends TemplateMailable
+{
+    /** @var string */
+    public $name;
+    
+    /** @var string */
+    public $email;
+
+    public function __construct(User $user)
+    {
+        $this->name = $user->name;
+        $this->email = $user->email;
+    }
+    
+    public function getHtmlLayout(): string
+    {
+        /**
+         * In your application you might want to fetch the layout from an external file or Blade view.
+         * 
+         * External file: `return file_get_contents(storage_path('mail-layouts/main.html'));`
+         * 
+         * Blade view: `return view('mailLayouts.main', $data)->render();`
+         */
+        
+        return '<header>Site name!</header>{{{ body }}}<footer>Copyright 2018</footer>';
+    }
+}
