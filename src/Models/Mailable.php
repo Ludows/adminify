e04c0a067550f50fa375c4d2de266166172f28c3
l.cointrel <l.cointrel@mediactive.fr>
2021-07-28 20:31:10 +0200

    Setting up Mailable Model

--- src/Models/Mailable.php
+++ src/Models/Mailable.php
@@ -3,9 +3,6 @@
 namespace Ludows\Adminify\Models;
 
 use Ludows\Adminify\Models\ClassicMail;
-use Spatie\Feed\FeedItem;
-use Spatie\Searchable\SearchResult;
-
 class Mailable extends ClassicMail
 {
 
