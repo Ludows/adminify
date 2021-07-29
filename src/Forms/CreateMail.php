d979c388cc91e7f462e9c5fe2ff7ad0527f63696
l.cointrel <l.cointrel@mediactive.fr>
2021-07-29 16:25:59 +0200

    Add create mail

--- src/Forms/CreateMail.php
+++ src/Forms/CreateMail.php
@@ -10,6 +10,30 @@ class CreateMail extends Form
     {
 
         //@todo
+        $this->add('mailable', 'select2', [
+            'empty_value' => '',
+            'withCreate' => false,
+            'modal' => '', // simple include
+            'choices' => $hydratorCat['categories'],
+            'selected' => $hydratorCat['selected'],
+            'attr' => ['multiple' => 'multiple'],
+            'label' => __('admin.form.categories_id'),
+            'select2options' => [
+                'placeholder' => __('admin.select_category'),
+                'multiple' => true,
+                'width' => '100%'
+            ]
+        ]);
+
+        $this->add('subject', 'text', [
+            'label_show' => false,
+            'attr' => ['placeholder' => __('admin.form.subject')],
+        ]);
+
+        $this->add('html_template', 'summernote', [
+            'label_show' => false,
+            'attr' => ['placeholder' => __('admin.form.html_template')],
+        ]);
         
     }
 }
