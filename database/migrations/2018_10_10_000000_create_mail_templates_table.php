55a1fd69c5496a3a0175d9a7fa9de3db60f6411c
l.cointrel <l.cointrel@mediactive.fr>
2021-07-29 09:00:01 +0200

    Adding migrations for create mailable tables

---
+++ database/migrations/2018_10_10_000000_create_mail_templates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMailTemplatesTable extends Migration
+{
+    public function up()
+    {
+
+        if(config('site-settings.multilang') == true) {
+            Schema::create('mail_templates', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('mailable');
+                $table->json('subject');
+                $table->json('html_template');
+                $table->longtext('text_template')->nullable();
+                $table->timestamps();
+            });
+        }
+        else {
+            Schema::create('mail_templates', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('mailable');
+                $table->text('subject')->nullable();
+                $table->longtext('html_template');
+                $table->longtext('text_template')->nullable();
+                $table->timestamps();
+            });
+        }
+        
+    }
+}
