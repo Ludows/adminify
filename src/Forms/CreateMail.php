96bdce79f0430a5d7583f7377bba3aaca8547eaa
l.cointrel <l.cointrel@mediactive.fr>
2021-07-29 16:40:01 +0200

    Mails

--- src/Forms/CreateMail.php
+++ src/Forms/CreateMail.php
@@ -8,19 +8,19 @@ class CreateMail extends Form
 {
     public function buildForm()
     {
+        $mailables = $this->getMailables();
 
         //@todo
         $this->add('mailable', 'select2', [
             'empty_value' => '',
             'withCreate' => false,
             'modal' => '', // simple include
-            'choices' => $hydratorCat['categories'],
-            'selected' => $hydratorCat['selected'],
-            'attr' => ['multiple' => 'multiple'],
-            'label' => __('admin.form.categories_id'),
+            'choices' => $mailables['mailables'],
+            'selected' => $mailables['selected'],
+            'label' => __('admin.form.mailable'),
             'select2options' => [
-                'placeholder' => __('admin.select_category'),
-                'multiple' => true,
+                'placeholder' => __('admin.select_mailable'),
+                'multiple' => false,
                 'width' => '100%'
             ]
         ]);
@@ -35,5 +35,28 @@ class CreateMail extends Form
             'attr' => ['placeholder' => __('admin.form.html_template')],
         ]);
         
+    }
+    public function getMailables() {
+        $m = app('App\Models\Mailables');
+
+        $mailables = $m->all();
+        $mails = [];
+        $selecteds = [];
+        $mailables = $m->all();
+        
+        foreach ($mailables as $mail) {
+            # code...
+            $mails[$mail->mailable] = $mail::getSubject();
+        }
+
+        $r = $this->getRequest();
+
+        if($r->exist('mail')) {
+            // on a une selection
+            $mailFetched = $r->mail;
+            $selecteds = [$mailFetched->mailable];
+        }
+
+        return [ 'mailables' => $mails, 'selected' => $selecteds];
     }
 }
