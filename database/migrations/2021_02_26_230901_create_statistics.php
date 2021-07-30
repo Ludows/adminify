2d5ec3cb3e06594b10e27ea4b415765340d8ea45
l.cointrel <l.cointrel@mediactive.fr>
2021-07-30 14:27:14 +0200

    Add statistics table

---
+++ database/migrations/2021_02_26_230901_create_statistics.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        // url 
+        // type => event / view 
+        // eventName => null,
+        // browser
+        // browser version
+        // navigator type
+        // date
+
+        Schema::create('statistics', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('url', 255);
+            $table->string('type', 255);
+            $table->string('event_name', 255);
+            $table->string('browser', 255);
+            $table->string('browser_version', 255);
+            $table->string('device_type', 255);
+            $table->date('visited_at');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statistics');
+    }
+}
