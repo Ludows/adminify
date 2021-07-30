63df278709832c29af67a4a8c02a4778d4e1c148
l.cointrel <l.cointrel@mediactive.fr>
2021-07-30 14:47:59 +0200

    Fix migration name

--- database/migrations/2021_02_26_230901_create_statistics.php
+++ database/migrations/2021_02_26_230901_create_statistics.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSeo extends Migration
+class CreateStatistics extends Migration
 {
     /**
      * Run the migrations.
