c3b6a2e951c72e3635985c9807792ede0228abb6
l.cointrel <l.cointrel@mediactive.fr>
2021-07-30 15:58:21 +0200

    RE re fix

--- routes/front.php
+++ routes/front.php
@@ -7,7 +7,7 @@ $headless = config('site-settings.headless');
 if(request()->segment(1) != $c['prefix'] || $headless) {
     Route::middleware('multilang.basic')->group(function ($router) {
         $router->get('/', 'Ludows\Adminify\Http\Controllers\Front\PageController@index')->name('pages.front.index');
-        $router->get('{slug}', 'Ludows\Adminify\Http\Controllers\Front\PageController@getPages')->where('slug', '([A-Za-z0-9\-\/]+)')->name('pages.front.show')->middleware('pages.blogpage');
+        $router->get('{slug}', 'Ludows\Adminify\Http\Controllers\Front\PageController@getPages')->where('slug', '([A-Za-z0-9\-\/]+)')->name('pages.front.show')->middleware('front.blogpage');
         $router->bind('slug', 'Ludows\Adminify\Http\Controllers\Front\PageController@handleSlug');
     });
 }
