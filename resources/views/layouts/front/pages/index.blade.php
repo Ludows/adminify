bd296a492b67334dc0e932166bdcc1fb234c59bd
l.cointrel <l.cointrel@mediactive.fr>
2021-07-31 10:42:45 +0200

    Show title

--- resources/views/layouts/front/pages/index.blade.php
+++ resources/views/layouts/front/pages/index.blade.php
@@ -14,17 +14,20 @@
     @if($type == "Post")
         @php
             $allowForm = true;
+            $showTitle = true;
             if($user == null) {
                 $allowForm = false;
             } 
             if(setting('no_comments') == 1 && $allowForm) {
                 $allowForm = false;
+                $showTitle = false;
             } 
             if($allowForm && $model->no_comments) {
                 $allowForm = false;
+                $showTitle = false;
             }
         @endphp
-        <comments ref="comments" lang='{{ $lang }}' :post_id="{{ $model->id }}" :allow_form="{{ $allowForm ? 'true' : 'false' }}" :user="{{ $user ?? '{}' }}" :comments='@json($model->commentsThree)'></comments>
+        <comments ref="comments" :show_title="{{ $showTitle ? 'true' : 'false' }}" lang='{{ $lang }}' :post_id="{{ $model->id }}" :allow_form="{{ $allowForm ? 'true' : 'false' }}" :user="{{ $user ?? '{}' }}" :comments='@json($model->commentsThree)'></comments>
     @endif
 @endsection
 
