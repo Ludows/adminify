5b0833efabf0f847ad1a6299c58b99448a5e20ae
l.cointrel <l.cointrel@mediactive.fr>
2021-08-03 00:33:29 +0200

    Add debug

--- publishable/resources/adminify/js/extensions/larabergField.js
+++ publishable/resources/adminify/js/extensions/larabergField.js
@@ -2,6 +2,7 @@ export default function LarabergInititalization(fields) {
     $.each(fields, function(i, el) {
         let textarea = $('#'+el.selector+' textarea');
         let laraberg =  Laraberg.init(textarea.attr('id'), el.options);
+        console.log('laraberg', laraberg)
 
         let modal_select = $('#modalSelectTemplate');
         let modal_save = $('#modalSaveTemplate');
