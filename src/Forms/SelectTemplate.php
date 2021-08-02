eb07b5b42c1f841c982056437c05ccfdcea91cc6
l.cointrel <l.cointrel@mediactive.fr>
2021-08-02 17:40:57 +0200

    Get templates

--- src/Forms/SelectTemplate.php
+++ src/Forms/SelectTemplate.php
@@ -4,17 +4,23 @@ namespace Ludows\Adminify\Forms;
 
 use Kris\LaravelFormBuilder\Form;
 
+use App\Models\Templates;
 class SelectTemplate extends Form
 {
     public function buildForm()
     {
+        $t = $this->getTemplates();
         // Add fields here...
         $this->add('items', 'select2', [
             'label' => __('admin.form.items'),
-            'attr' => [
-            ]
+            'choice' => $t['templates']
         ]);
         $this->add('submit', 'submit', ['label' => __('admin.add') , 'attr' => ['class' => 'btn btn-default']]);
 
+    }
+    public function getTemplates() {
+        $templates = Templates::get()->pluck('title' ,'id');
+
+        return [ 'templates' => $templates->all(), 'selected' => ''];
     }
 }
