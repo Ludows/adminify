de4f0ab4bfd1e557ce7e238bfcf0f7aafd72414e
l.cointrel <l.cointrel@mediactive.fr>
2021-08-02 15:59:48 +0200

    Js Update

--- publishable/resources/adminify/js/extensions/larabergField.js
+++ publishable/resources/adminify/js/extensions/larabergField.js
@@ -1,6 +1,6 @@
 export default function LarabergInititalization(fields) {
     $.each(fields, function(i, el) {
         let textarea = $('#'+el.selector+' textarea');
-        let laraberg =  Laraberg.init(textarea.attr('id'), { laravelFilemanager: true })
+        let laraberg =  Laraberg.init(textarea.attr('id'), el.options)
     })
 }
