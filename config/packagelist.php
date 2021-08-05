8d203e20f591137fadc32c37cc23c43c41502ee0
l.cointrel <l.cointrel@mediactive.fr>
2021-08-05 08:12:31 +0200

    Verify Middleware

--- config/packagelist.php
+++ config/packagelist.php
@@ -302,7 +302,7 @@ return array(
                         'admin.deletemedia' => \App\Http\Middleware\DeleteMedia::class,
                         'admin.fullmode' => \App\Http\Middleware\DisplayFullmode::class,
                         'admin.seo' => \App\Http\Middleware\BackendSeo::class,
-                        'api.verify' => \App\Http\Middleware\VerifyApiToken::class
+                        'api.verify_token' => \App\Http\Middleware\VerifyApiToken::class
                     ],
                     'web' => [
                         \App\Http\Middleware\ShareCurrentUser::class
