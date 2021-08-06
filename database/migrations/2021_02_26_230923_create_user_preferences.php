bb1e60d5bf8aad3093a8f4727d8bf10ab62b2e65
l.cointrel <l.cointrel@mediactive.fr>
2021-08-06 15:42:14 +0200

    Fix migrate

--- database/migrations/2021_02_26_230923_create_user_preferences.php
+++ database/migrations/2021_02_26_230923_create_user_preferences.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserPreference extends Migration
+class CreateUserPreferences extends Migration
 {
     /**
      * Run the migrations.
