a54eae3736e54164ce9ef72d5cbab32d677ffff9
l.cointrel <l.cointrel@mediactive.fr>
2021-08-06 16:16:49 +0200

    Setting Up profile page

--- resources/views/layouts/admin/pages/profile.blade.php
+++ resources/views/layouts/admin/pages/profile.blade.php
@@ -1,11 +1,34 @@
 @extends('adminify::layouts.admin.app')
 
 @section('content')
-    @if(isset($form))
-        {!! form($form) !!}
-    @endif
+    
+    @include('adminify::layouts.admin.headers.topPageListing')
+
+    <div>
+        <div class="container-fluid mt--7">
+            <div class="row">
+                <div class="col-12">
+    
+                    @if(isset($form))
+                        {!! form($form) !!}
+                    @endif
+                    
+                </div>
+        
+            </div>
+        
+            @include('adminify::layouts.admin.footers.auth')
+        </div>
+    </div>
+    
 @endsection
 
 @push('js')
    
 @endpush
+
+
+
+
+
+
