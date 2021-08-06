9a8ac380fee6069e2317392dfb4c5f1ad618f776
l.cointrel <l.cointrel@mediactive.fr>
2021-08-06 15:35:43 +0200

    Add field for show topbar or not

--- src/Forms/ShowProfile.php
+++ src/Forms/ShowProfile.php
@@ -8,6 +8,15 @@ class ShowProfile extends Form
 {
     public function buildForm()
     {
-       
+        $this->add('topbar', 'checkbox', [
+            'label' => __('admin.form.topbar'),
+            'wrapper' => ['class' => 'custom-control custom-control-alternative custom-checkbox'],
+            'attr' => ['class' => 'custom-control-input'],
+            'label_attr' => ['class' => 'custom-control-label text-muted'],
+        ]);
+        $this->add('submit', 'submit', [
+            'label' => __('admin.form.submit'),
+            'attr' => ['class' => 'btn btn-primary my-4'],
+        ]);
     }
 }
