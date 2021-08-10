2bac3ed186bc3a5c9ce9625b84188e54f153ada2
l.cointrel <l.cointrel@mediactive.fr>
2021-08-10 14:05:31 +0200

    Clean code. We dealing with the generator

--- src/Commands/createCrud.php
+++ src/Commands/createCrud.php
@@ -4,6 +4,7 @@ namespace Ludows\Adminify\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
 
 class CreateCrud extends Command
 {
@@ -12,7 +13,7 @@ class CreateCrud extends Command
      *
      * @var string
      */
-    protected $signature = 'adminify:resources {model, typeModel}';
+    protected $signature = 'adminify:resources {model, typeModel} {--fields=}';
 
     /**
      * The console command description.
@@ -41,21 +42,31 @@ class CreateCrud extends Command
 
         $model = $this->argument('model');
         $typeModel = $this->argument('typeModel') == 'content' ? 'content' : 'classic';
+        $fields = $this->option('fields');
+
+        $model = Str::title($model ?? '');
+
+        // ensure his plurial name
+        $model = Str::plural($model);
+
 
         $this->info('Do DB file Creation for your entity...');
-        Artisan::call('adminify:migration_file', [
-            'model' => $model 
+        Artisan::call('generate:migration', [
+            'name' => 'create_'. Str::lower($model) .'_table',
+            'schema' => $fields ?? [],
+            'stub' => ''
         ]);
 
         $this->info('Create Controller for your entity...');
-        Artisan::call('adminify:controller', [
-            'model' => $model 
+        Artisan::call('generate:controller', [
+            'name' => Str::singular($model),
+            'stub' => ''
         ]);
 
         $this->info('Create Model for your entity...');
-        Artisan::call('adminify:model', [
-            'model' => $model ,
-            'type' => $typeModel
+        Artisan::call('generate:model', [
+            'name' => Str::singular($model) ,
+            'stub' => $typeModel
         ]);
 
         $this->info('Create Table Listing for your entity...');
@@ -75,11 +86,12 @@ class CreateCrud extends Command
 
         $this->info('Create CRUD Forms for your entity...');
         Artisan::call('adminify:form', [
-            'model' => $model 
+            'model' => $model,
+            'fields' => $fields
         ]);
 
         $this->info('Create Repository for your entity...');
-        Artisan::call('adminify:repository', [
+        Artisan::call('generate:repository', [
             'model' => $model 
         ]);
         
