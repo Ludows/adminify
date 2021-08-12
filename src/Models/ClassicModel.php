dc89b7db7263d611b10c4c37e5def3decece9b1a
l.cointrel <l.cointrel@mediactive.fr>
2021-08-12 11:47:01 +0200

    Remove OnBooted Model

--- src/Models/ClassicModel.php
+++ src/Models/ClassicModel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
-use Ludows\Adminify\Traits\OnBootedModel;
+// use Ludows\Adminify\Traits\OnBootedModel;
 use Spatie\Translatable\HasTranslations;
 use Ludows\Adminify\Traits\MultilangTranslatableSwitch;
 use Ludows\Adminify\Traits\Helpers;
@@ -24,7 +24,7 @@ use Spatie\Feed\FeedItem;
 abstract class ClassicModel extends Model implements Searchable, Feedable
 {
     use HasFactory;
-    use OnBootedModel;
+    // use OnBootedModel;
     // use Urlable;
     use HasTranslations;
     use MultilangTranslatableSwitch;
