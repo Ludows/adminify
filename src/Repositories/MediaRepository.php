935dbe92e59304ad36e8a9fa2f76f2d615985ce2
l.cointrel <l.cointrel@mediactive.fr>
2021-08-17 09:22:08 +0200

    Path

--- src/Repositories/MediaRepository.php
+++ src/Repositories/MediaRepository.php
@@ -8,7 +8,7 @@ class MediaRepository extends BaseRepository
 {   
     public function getMimeTypeProcess($model, $formValues, $type) {
         
-        $path = $model->path;
+        $path = $model->getFullPath($model->src) . '/' . $model->src;
 
         $model->mime_type = mime_content_type($path);
     }
