814645aad36c5476903e162c6e6ef8578aedf876
l.cointrel <l.cointrel@mediactive.fr>
2021-08-22 12:42:55 +0200

    Fixing Url Updates

--- src/Hooks/HandleSettings.php
+++ src/Hooks/HandleSettings.php
@@ -22,16 +22,16 @@ class HandleSettings extends HookInterface {
             if($modelHook->type == 'homepage') {
                 $m = Url::where('from_model', 'App\Models\Page')->where('model_id', (int) $handles['homepage'])->get()->first();
                 $key = 'is_homepage';
-                $val = $handles['homepage'];
+                $val = $handles['homepage'] != null ? (boolean) $handles['homepage'] : false;
             }
             if($modelHook->type == 'blogpage') {
                 $m = Url::where('from_model', 'App\Models\Page')->where('model_id', (int) $handles['blogpage'])->get()->first();
                 $key = 'is_blogpage';
-                $val = $handles['blogpage'];
+                $val = $handles['blogpage'] != null ? (boolean) $handles['blogpage'] : false;
             }
 
             if($m != null) {
-                $m->fill([
+                $m->update([
                     $key => $val
                 ]);
 
