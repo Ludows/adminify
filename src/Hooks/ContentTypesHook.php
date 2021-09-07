83bbb925bf9376a5837cbc354f2ed5a3a59c47a8
l.cointrel <l.cointrel@mediactive.fr>
2021-09-07 08:28:50 +0200

    Deleted hook Fixes

--- src/Hooks/ContentTypesHook.php
+++ src/Hooks/ContentTypesHook.php
@@ -8,8 +8,11 @@ class ContentTypesHook extends HookInterface {
     public function handle($hookName,$datas = null) {
         //data is the model passed
         $model = $datas;
+        $allowed_hooks = ['model:creating', 'model:created', 'model:updating', 'model:updated'];
+        $only_for_deleting = ['model:deleting'];
 
-        if($model != null) {
+
+        if($model != null && in_array($hookName, $allowed_hooks)) {
             if(is_urlable_model($model) && $model->allowSitemap) {
                 $this->syncronizeUrl($model);
                 $this->syncToCache($model);
@@ -20,9 +23,17 @@ class ContentTypesHook extends HookInterface {
             }
         }
 
+        if(in_array($hookName, $only_for_deleting)) {
+            $this->syncToCache($model, true);
+            $this->syncronizeUrl($model, true);
+            if(is_sitemapable_model($model) && $model->allowSitemap) {
+                $this->loadGenerateSitemap($model);
+            }
+        }
+
     }
-    public function syncronizeUrl($context) {
-        return $this->walkThroughtParents($context, $context->id, $context->id);
+    public function syncronizeUrl($context, $delete = false) {
+        return $delete == false ? $this->walkThroughtParents($context, $context->id, $context->id) : $context->deleteUrl([], true);
     }
     public function walkThroughtParents($context, $from = null,  $baseOrder = 1) {
         $parentable = $context->parent_id;
@@ -59,13 +70,13 @@ class ContentTypesHook extends HookInterface {
             ]);
         }
     }
-    public function syncToCache($context) {
+    public function syncToCache($context, $delete = false) {
         $url = $context->url;
         if($url != null) {
 
             $isHomePage = is_homepage($context);
 
-            $context->encryptToCache( $isHomePage ? 'homepage' : join('.', $url) );
+            $delete == false ? $context->encryptToCache( $isHomePage ? 'homepage' : join('.', $url) ) : $context->forgetToCache( $isHomePage ? 'homepage' : join('.', $url ));
         }
     }
     
