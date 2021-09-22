eff5cc735be518fba18838298f65f19188e5b068
l.cointrel <l.cointrel@mediactive.fr>
2021-09-22 19:53:15 +0200

    Rerun all translation when Translation model has created entity

--- src/Hooks/OnCreatedHook.php
+++ src/Hooks/OnCreatedHook.php
@@ -3,10 +3,15 @@
 namespace Ludows\Adminify\Hooks;
 
 use Ludows\Adminify\Libs\HookInterface;
-
+use Illuminate\Support\Facades\Artisan;
 class OnCreatedHook extends HookInterface {
     public function handle($hookName,$datas = null) {
         //data is the model passed
+        $m = $datas;
+        if(!empty($m) && $m instanceof \App\Models\Translations) {
+            Artisan::call('adminify:translations', [
 
+            ]);
+        }
     }
 }
