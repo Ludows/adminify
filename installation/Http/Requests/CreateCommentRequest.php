a00c3a6610b5fe6253ed4d8495b757950bb53f65
l.cointrel <l.cointrel@mediactive.fr>
2021-10-04 16:39:46 +0200

    Delete installation old folder

--- installation/Http/Requests/CreateCommentRequest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Requests;
-
-use Ludows\Adminify\Http\Requests\CreateCommentRequest as CreateCommentRequestRequestBase;
-
-class CreateCommentRequest extends CreateCommentRequestRequestBase
-{}
-
