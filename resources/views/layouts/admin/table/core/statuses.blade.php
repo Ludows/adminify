986d46dc92f68d9bcddf9e82c84accd098a1e22c
l.cointrel <l.cointrel@mediactive.fr>
2021-10-06 18:08:56 +0200

    Fix status

--- resources/views/layouts/admin/table/core/statuses.blade.php
+++ resources/views/layouts/admin/table/core/statuses.blade.php
@@ -1,6 +1,6 @@
 <div class="col">
     <select class="form-control js-select-status">
-        <option value="-1">{{ __('admin.table.modules.statuses.select_status') }}</option>
+        <option value="1">{{ __('admin.table.modules.statuses.select_status') }}</option>
         @foreach ($statuses as $status)
             <option value="{{ $status->id }}">{{ __('admin.table.modules.statuses.'.$status->name) }}</option>
         @endforeach
