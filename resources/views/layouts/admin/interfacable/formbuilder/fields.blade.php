dfc09ae7a3c4edbc39a3428ccbf5015a73b5c209
l.cointrel <l.cointrel@mediactive.fr>
2021-10-07 19:36:21 +0200

    Fix typo

--- resources/views/layouts/admin/interfacable/formbuilder/fields.blade.php
+++ resources/views/layouts/admin/interfacable/formbuilder/fields.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-header">
-        {{ _i('adminify.formbuilder.fields') }}
+        {{ __('adminify.formbuilder.fields') }}
     </div>
     <div class="card-body">
         <div class="row js-dropzone">
