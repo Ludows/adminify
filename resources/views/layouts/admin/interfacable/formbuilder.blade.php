ee6f7f451b9b3aeffe3596d0ba067c92ab63dbc8
l.cointrel <l.cointrel@mediactive.fr>
2021-10-07 17:39:45 +0200

    Making in place the blocks

--- resources/views/layouts/admin/interfacable/formbuilder.blade.php
+++ resources/views/layouts/admin/interfacable/formbuilder.blade.php
@@ -1,2 +1,15 @@
-FORMBUILDER PART
 @include('adminify::layouts.admin.headers.topPageListing')
+
+<div class="container mt--7">
+    <div class="row">
+        <div class="col-lg-8 col-12">
+            {!! $blocks['form-block']->render() !!}
+        </div>
+        <div class="col-lg-4 col-12">
+            {!! $blocks['fields-block']->render() !!}
+        </div>
+        <div id="templateFormField" class="d-none">
+            {!! $blocks['template-block']->render() !!}
+        </div>
+    </div>
+</div>
