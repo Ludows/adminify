cb413cdd30d215404135ef574a4430337edc16ea
l.cointrel <l.cointrel@mediactive.fr>
2021-10-07 11:57:22 +0200

    Create form entries table

---
+++ database/migrations/2020_12_17_193102_create_form_entries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+            Schema::create('form_entries', function (Blueprint $table) {
+                $table->bigIncrements('id')->unsigned();
+                $table->string('field_name');
+                $table->string('content');
+                $table->bigInteger('form_id');
+                $table->timestamps();
+            });
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_entries');
+    }
+}
