f8b1304abfb6c509fbc27a8badef8640506fb254
l.cointrel <l.cointrel@mediactive.fr>
2021-10-20 18:03:33 +0200

    Add typed fields

--- src/Interfacable/Blocks/FormBuilder/FieldsBlock.php
+++ src/Interfacable/Blocks/FormBuilder/FieldsBlock.php
@@ -9,6 +9,19 @@ use Illuminate\Support\Arr;
 
 
 class FieldsBlock extends InterfacableBlock {
+    public function __construct() {
+        parent::__construct();
+        $this->excludes = [
+            'entity',
+            'form',
+            'submit',
+            'image',
+            'choice',
+            'radio',
+            'select',
+            'checkbox'
+        ];
+    }
     public static function getNamedBlock() {
         return 'Fields Block';
     }
@@ -31,6 +44,27 @@ class FieldsBlock extends InterfacableBlock {
         $configBuilder = array_keys(config('laravel-form-builder.custom_fields'));
 
         $concat =  Arr::flatten(array_merge($constants, $configBuilder));
+
+        // gestion 
+
+        $types_choices = [
+            'radio', 
+            'select', 
+            'checkbox'
+        ];
+
+        if(isset($concat['choice'])) {
+            foreach ($types_choices as $types_choice) {
+                # code...
+                $concat[] = 'choice.'.$types_choice;
+            }
+        }
+
+        $concat = array_filter($concat, function($v) {
+            if(!in_array($v, $this->excludes)) {
+                return $v;
+            }
+        });
 
         return [
             'fields' => $concat
