8c87b182cf9a6266abaf210f60b70785c97b14d9
l.cointrel <l.cointrel@mediactive.fr>
2021-11-03 11:02:58 +0100

    Sync method for relation forms

--- src/Repositories/FormsRepository.php
+++ src/Repositories/FormsRepository.php
@@ -106,7 +106,7 @@ class FormsRepository extends BaseRepository
             }
 
             if(count($attachements) > 0) {
-                $model->fields()->attach($attachements);
+                $model->fields()->syncWithoutDetaching($attachements);
             }
         }
 
