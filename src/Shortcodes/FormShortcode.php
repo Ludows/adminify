b679e4ec69b6c8f80a0a16c9782f721327be91a8
l.cointrel <l.cointrel@mediactive.fr>
2021-11-04 22:59:36 +0100

    Add shortcode for forms

--- src/Shortcodes/FormShortcode.php
+++ src/Shortcodes/FormShortcode.php
@@ -7,6 +7,24 @@ use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 
 class FormShortcode extends ShortcodeBaseRender {
     public function render(ShortcodeInterface $s) {
-        dd('form shortcode');
+        //dd('form shortcode');
+
+        $adapter = null;
+        $idParam = $s->getParameter('id');
+        $elementParam = $s->getParameter('element');
+
+        if(empty($idParam) && empty($elementParam)) {
+            return '';
+        }
+
+        if($idParam != null) {
+            $adapter = (int) $idParam;
+        }
+        else {
+            $adapter = $elementParam;
+        }
+
+        return generate_form($adapter);
+
     }
 }
