1294751e6896788f630687f4398b127e0b2992ce
l.cointrel <l.cointrel@mediactive.fr>
2021-11-10 12:21:26 +0100

    Prevent dupplicated keys in db

--- src/Repositories/FormConfirmationRepository.php
+++ src/Repositories/FormConfirmationRepository.php
@@ -8,9 +8,14 @@ class FormConfirmationRepository extends BaseRepository
 {
    public function afterRun($model, $formValues,  $type) {
 
-        //retrieve the form model from request
-        $f = request()->model;
+         //retrieve the form model from request
+         $f = request()->model;
 
+         // prevent dupplicated ids in db 
+         if($type == "update") {
+            $f->confirmation()->detatch();
+         }
+        
         // prepare to insert the relationship
         $f->confirmation()->attach([$model->id]);
 
