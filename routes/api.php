eeeaaf79bef9408bf8962c6b20da6c5b76fe2ac2
l.cointrel <l.cointrel@mediactive.fr>
2021-11-12 10:28:01 +0100

    Add fixes for api defaults routes

--- routes/api.php
+++ routes/api.php
@@ -12,19 +12,19 @@ if(isset($c) && $c['enable']) {
     Route::post('/token/refresh', '\Ludows\Adminify\Http\Controllers\Api\TokenController@refreshToken')->name('api.token.refresh');
 
     if(isset($c['post']) && $c['post']) {	
-	    Route::resource('posts', 'App\Adminify\Http\Controllers\Api\PostController', ['except' => ['show']] )->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
+	    Route::resource('posts', 'App\Adminify\Http\Controllers\Api\PostController', ['except' => ['show']] )->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic'])->name('api.posts');
     }
     
     if(isset($c['media']) && $c['media']) {
-        Route::resource('medias', 'App\Adminify\Http\Controllers\Api\MediaController', ['except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
+        Route::resource('medias', 'App\Adminify\Http\Controllers\Api\MediaController', ['except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic'])->name('api.medias');
     }
 	
     if(isset($c['category']) && $c['category']) {
-        Route::resource('categories', 'App\Adminify\Http\Controllers\Api\CategoryController', ['except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
+        Route::resource('categories', 'App\Adminify\Http\Controllers\Api\CategoryController', ['except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic'])->name('api.categories');
     }
 
     if(isset($c['page']) &&  $c['page']) {	
-        Route::resource('pages', 'Api\Adminify\Http\Controllers\Api\PageController', ['except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
+        Route::resource('pages', 'Api\Adminify\Http\Controllers\Api\PageController', ['except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic'])->name('api.pages');
     }
     
     if(isset($c['menu']) && $c['menu']) {
@@ -34,52 +34,22 @@ if(isset($c) && $c['enable']) {
         // Route::post('menus/check-entity/{id}/{type}', 'Ludows\Adminify\Http\Controllers\Back\MenuController@checkEntity')->name('menus.checkEntity');
     }
 
-    
-    if(isset($c['templates_content']) && $c['templates_content']) {
-        // Route::resource('templates', 'Ludows\Adminify\Http\Controllers\Back\TemplatesController', ['except' => ['show']]);
-        // Route::post('templates/content', 'Ludows\Adminify\Http\Controllers\Back\TemplatesController@setContent')->name('templates.setcontent');
-    }
-
     if(isset($c['comment']) && $c['comment']) {
-        Route::resource('comments', 'App\Adminify\Http\Controllers\Api\CommentController', ['except' => ['show' ]])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
+        Route::resource('comments', 'App\Adminify\Http\Controllers\Api\CommentController', ['except' => ['show' ]])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic'])->name('api.comments');
     }
 
     if(isset($c['setting']) && $c['setting']) {
         // Route::resource('settings', 'Ludows\Adminify\Http\Controllers\Back\SettingsController', ['except' => ['show', 'update', 'delete', 'edit']]);
     }
-    
-    if(isset($c['user']) && $c['user']) {
-        // Route::resource('users', 'Ludows\Adminify\Http\Controllers\Back\UserController', ['except' => ['show']]);
-
-        // Route::get('users/{user}/profile/', 'Ludows\Adminify\Http\Controllers\Back\UserController@showProfile')->name('users.profile.edit');
-
-        // Route::post('users/{user}/profile/save', 'Ludows\Adminify\Http\Controllers\Back\UserController@saveProfile')->name('users.profile.store');
-    }
 
     if(isset($c['key_translation']) && $c['key_translation']) {
-        Route::resource('traductions', 'App\Adminify\Http\Controllers\Api\TranslationsController', ['except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
+        Route::resource('traductions', 'App\Adminify\Http\Controllers\Api\TranslationsController', ['except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic'])->name('api.translations');
     }
 
-    if(isset($c['tag']) && $c['tag']) {
-        // Route::resource('tags', 'Ludows\Adminify\Http\Controllers\Back\TagsController', ['except' => ['show']]);
-    }
     if(isset($c['email']) && $c['email']) {
         // Route::resource('mails', 'Ludows\Adminify\Http\Controllers\Back\MailsController', ['except' => ['show']]);
 
         // Route::post('mails/send/{mail}', 'Ludows\Adminify\Http\Controllers\Back\MailsController@send')->name('mails.send');
-    }
-
-    if(is_multilang()) {
-        // Route::resource('savetraductions', 'Ludows\Adminify\Http\Controllers\Back\SaveTranslationsController', ['except' => ['show', 'create', 'store', 'index', 'destroy']]);
-    }
-
-    if(isset($c['seo']) && $c['seo']) {	
-
-        // Route::get('seo/{type}/{id}', 'Ludows\Adminify\Http\Controllers\Back\SeoController@edit')->name('seo.edit');
-
-        // Route::put('seo/{type}/{id}', 'Ludows\Adminify\Http\Controllers\Back\SeoController@update')->name('seo.update');
-        // Route::patch('seo/{type}/{id}', 'Ludows\Adminify\Http\Controllers\Back\SeoController@update')->name('seo.update');
-
     }
 
     if(isset($c['form']) && $c['form']) {
@@ -96,12 +66,14 @@ if(isset($c) && $c['enable']) {
         }
     }
     if(count($c['customRoutes']) > 0) {
+        $iterator_custom_routes = 0;
         foreach ($c['customRoutes'] as $arrayValues) {
             # code...
             foreach ($arrayValues['verbs'] as $verb) {
                 # code...
-                Route::{$verb}($arrayValues['route'], $arrayValues['controller'])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
+                Route::{$verb}($arrayValues['route'], $arrayValues['controller'])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic'])->name('api.custom_route_'.$iterator_custom_routes);
             }
+            $iterator_custom_routes++;
         }
     }
 
