79395db3ce97ca0dfe75064a21299e3f02451e02
l.cointrel <l.cointrel@mediactive.fr>
2021-11-12 11:08:38 +0100

    Route api fix

--- routes/api.php
+++ routes/api.php
@@ -6,55 +6,69 @@ $c = config('site-settings.restApi');
 $features = config('site-settings.enables_features');
 
 if(isset($c) && $c['enable']) {
-    
+
     Route::get('/', '\Ludows\Adminify\Http\Controllers\Api\ListingController@index')->name('routelist');
     Route::post('/token', '\Ludows\Adminify\Http\Controllers\Api\TokenController@getToken')->name('api.token');
     Route::post('/token/verify', '\Ludows\Adminify\Http\Controllers\Api\TokenController@verifyToken')->name('api.token.verify');
     Route::post('/token/refresh', '\Ludows\Adminify\Http\Controllers\Api\TokenController@refreshToken')->name('api.token.refresh');
 
-    Route::prefix($c['prefix'])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic'])->group( function () use ($c, $features) {
-        if(isset($features['post']) && $features['post']) {	
-            Route::resource('posts', 'App\Adminify\Http\Controllers\Api\PostController', ['except' => ['show']] );
+    // Route::prefix($c['prefix'])->group( function () use ($c, $features) {
+
+
+        if(isset($features['post']) && $features['post']) {
+            Route::resource('posts', 'App\Adminify\Http\Controllers\Api\PostController', [
+                'as' => $c['prefix'],
+                'except' => ['show']] )->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
         }
-        
+
         if(isset($features['media']) && $features['media']) {
-            Route::resource('medias', 'App\Adminify\Http\Controllers\Api\MediaController', ['except' => ['show']]);
+            Route::resource('medias', 'App\Adminify\Http\Controllers\Api\MediaController', [
+                'as' => $c['prefix'],
+                'except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
         }
-        
+
         if(isset($features['category']) && $features['category']) {
-            Route::resource('categories', 'App\Adminify\Http\Controllers\Api\CategoryController', ['except' => ['show']]);
+            Route::resource('categories', 'App\Adminify\Http\Controllers\Api\CategoryController', [
+                'as' => $c['prefix'],
+                'except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
         }
-    
-        if(isset($features['page']) &&  $features['page']) {	
-            Route::resource('pages', 'Api\Adminify\Http\Controllers\Api\PageController', ['except' => ['show']]);
+
+        if(isset($features['page']) &&  $features['page']) {
+            Route::resource('pages', 'App\Adminify\Http\Controllers\Api\PageController', [
+                'as' => $c['prefix'],
+                'except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
         }
-        
+
         if(isset($features['menu']) && $features['menu']) {
-            
+
         }
-    
+
         if(isset($features['comment']) && $features['comment']) {
-            Route::resource('comments', 'App\Adminify\Http\Controllers\Api\CommentController', ['except' => ['show' ]]);
+            Route::resource('comments', 'App\Adminify\Http\Controllers\Api\CommentController', [
+                'as' => $c['prefix'],
+                'except' => ['show' ]])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
         }
-    
+
         if(isset($features['setting']) && $features['setting']) {
 
         }
-    
+
         if(isset($features['key_translation']) && $features['key_translation']) {
-            Route::resource('traductions', 'App\Adminify\Http\Controllers\Api\TranslationsController', ['except' => ['show']]);
+            Route::resource('traductions', 'App\Adminify\Http\Controllers\Api\TranslationsController', [
+                'as' => $c['prefix'],
+                'except' => ['show']])->middleware(['api.verify_token', 'api.verify_abilities', 'multilang.basic']);
         }
-    
+
         if(isset($features['email']) && $features['email']) {
-    
+
         }
-    
+
         if(isset($features['form']) && $features['form']) {
 
         }
-    });
+    // });
 
-    
+
     //$config crud
     if(count($c['crud']) > 0) {
         foreach ($c['crud'] as $key => $classValue) {
