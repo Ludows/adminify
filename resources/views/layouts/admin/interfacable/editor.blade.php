94b0a8857b84d9a84b198b103faccc4727f2c7f4
l.cointrel <l.cointrel@mediactive.fr>
2021-11-13 21:59:12 +0100

    Design editor

--- resources/views/layouts/admin/interfacable/editor.blade.php
+++ resources/views/layouts/admin/interfacable/editor.blade.php
@@ -1,4 +1,10 @@
-MY EDITOR WORKPLACE
+<div class="adminify_editor">
 
+{!! $blocks['topbar-editor-block']->render() !!}
 {!! $blocks['sidebar-block']->render() !!}
-{!! $blocks['renderer-block']->render() !!}
+
+<div class="render_zone">
+    {!! $blocks['renderer-block']->render() !!}
+</div>
+
+</div>
