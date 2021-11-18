eb05b7de79f92e5028d696bca2cdb4cb486bb7f6
l.cointrel <l.cointrel@mediactive.fr>
2021-11-18 10:40:04 +0100

    Add some method control to editor

--- src/Http/Controllers/Back/EditorController.php
+++ src/Http/Controllers/Back/EditorController.php
@@ -36,68 +36,8 @@ class EditorController extends Controller
         $this->middleware(['permission:read|edit_pages'], ['only' => ['edit', 'update']]);
         $this->middleware(['permission:read|delete_pages'], ['only' => ['destroy']]);
     }
-    /**
-        * Display a listing of the resource.
-        *
-        * @return Response
-        */
-        public function index(Request $request)
-        {
-            
-        }
-
-        /**
-            * Show the form for creating a new resource.
-            *
-            * @return Response
-            */
-        public function create(FormBuilder $formBuilder)
-        {
-            
-        }
-
-        /**
-            * Store a newly created resource in storage.
-            *
-            * @return Response
-            */
-        public function store(CreateMailRequest $request)
-        {
-            
-        }
-
-        /**
-            * Show the form for editing the specified resource.
-            *
-            * @param  int  $id
-            * @return Response
-            */
-        public function edit(FormBuilder $formBuilder, Mailables $mail, Request $request)
-        {
-            
-        }
-
-        /**
-            * Update the specified resource in storage.
-            *
-            * @param  int  $id
-            * @return Response
-            */
-        public function update(Mailables $mail, UpdateMailRequest $request)
-        {
-            
-            
-        }
-
-        /**
-            * Remove the specified resource from storage.
-            *
-            * @param  int  $id
-            * @return Response
-            */
-        public function destroy(Mailables $mail)
-        {
-            
-
-        }
+    public function addWidget() {}
+    public function removeWidget() {}
+    public function autosave() {}
+    
 }
