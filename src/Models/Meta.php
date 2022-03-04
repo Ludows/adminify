0236fa3501590b8f0f711c315762fa19c78e1fdb
l.cointrel <l.cointrel@mediactive.fr>
2022-03-04 15:45:04 +0100

    Fix url

--- src/Models/Meta.php
+++ src/Models/Meta.php
@@ -38,7 +38,7 @@ class Meta extends ClassicModel
 
     public function getLinks($menuBuilder, $arrayDatas) {
         if($arrayDatas['user']->hasPermissionTo('create_metas') && $arrayDatas['features']['metas']) {
-            $menuBuilder->add( Link::to( $arrayDatas['multilang'] ? '/admin/metas?lang='. $arrayDatas['lang'] : '/admin/menus', '<i class="ni ni-single-copy-04"></i> '.__('admin.menuback.metas'))->setParentAttribute('class', 'nav-item')->addClass('nav-link') );
+            $menuBuilder->add( Link::to( $arrayDatas['multilang'] ? '/admin/metas?lang='. $arrayDatas['lang'] : '/admin/metas', '<i class="ni ni-single-copy-04"></i> '.__('admin.menuback.metas'))->setParentAttribute('class', 'nav-item')->addClass('nav-link') );
             // $menuBuilder->add( Link::to( $multilang ? '/admin/tags?lang='.$lang : '/admin/tags', '<i class="ni ni-single-copy-04"></i> '.__('admin.tags.index'))->setParentAttribute('class', 'nav-item')->addClass('nav-link') );
         }
     }
