a799fe0aee07cd236a260d691e80776f3c39b84d
l.cointrel <l.cointrel@mediactive.fr>
2022-03-08 15:54:10 +0100

    Add view_name in db

--- database/migrations/2020_12_21_145710_create_groupmetas_table.php
+++ database/migrations/2020_12_21_145710_create_groupmetas_table.php
@@ -20,6 +20,7 @@ class CreateGroupMetasTable extends Migration
                 $table->string('slug', 255);
             }
             $table->string('named_class', 255);
+            $table->string('view_name', 255);
             $table->bigInteger('user_id')->unsigned();
         });
     }
