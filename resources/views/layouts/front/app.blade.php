8432b311f300106683d25766a303cc3e87d91244
l.cointrel <l.cointrel@mediactive.fr>
2022-03-09 14:37:16 +0100

    Clean front end js and css calls

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ lang() }}">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <!-- Favicon -->
+        <link href="{{ asset() }}/favicon.png" rel="icon" type="image/png">
         {!! SEO::generate() !!}
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Favicon -->
-        <link href="{{ asset('adminify') }}/img/brand/favicon.png" rel="icon" type="image/png">
+       
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+        {{-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet"> --}}
         <!-- Extra details for Live View on GitHub Pages -->
 
         <!-- Icons -->
-        <link href="{{ asset('adminify') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
-        <link href="{{ asset('adminify') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+        {{-- <link href="{{ asset('adminify') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet"> --}}
+        {{-- <link href="{{ asset('adminify') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet"> --}}
         <!-- Argon CSS -->
-        <link type="text/css" href="{{ asset('adminify/front') }}/css/argon.css?v=1.0.0" rel="stylesheet">
-        <link type="text/css" href="{{ asset('adminify/front') }}/css/front.css?v=1.0.0" rel="stylesheet">
+        {{-- <link type="text/css" href="{{ asset('adminify/front') }}/css/argon.css?v=1.0.0" rel="stylesheet"> --}}
+        {{-- <link type="text/css" href="{{ asset('adminify/front') }}/css/front.css?v=1.0.0" rel="stylesheet"> --}}
         @laravelPWA
-        @stack('css')
         {!! Assets::group('frontend')->css() !!}
+        @stack('css')
     </head>
     <body class="{{ $class ?? '' }}">
         <main id="app">
@@ -49,9 +49,9 @@
             </div>
         </main>
         {{--  <script src="{{ asset('argon') }}/vendor/jquery/dist/jquery.min.js"></script>  --}}
-        <script src="{{ asset('myuploads') }}/routes.js"></script>
-        <script src="{{ asset('adminify/front') }}/js/app.js"></script>
-        <script src="{{ asset('myuploads') }}/traductions-{{ str_replace('_', '-', app()->getLocale()) }}.js"></script>
+        {{-- <script src="{{ asset('myuploads') }}/routes.js"></script> --}}
+        {{-- <script src="{{ asset('adminify/front') }}/js/app.js"></script> --}}
+        {{-- <script src="{{ asset('myuploads') }}/traductions-{{ str_replace('_', '-', app()->getLocale()) }}.js"></script> --}}
         {{--  <script src="{{ asset('argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>  --}}
 
         @stack('modales')
