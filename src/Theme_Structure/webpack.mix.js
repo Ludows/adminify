226f7ebd2ef39eacfd240adbcad53c83e748cb1b
l.cointrel <l.cointrel@mediactive.fr>
2022-08-01 16:50:26 +0200

    Rework front handling assets

--- src/Theme_Structure/webpack.mix.js
+++ src/Theme_Structure/webpack.mix.js
@@ -1,3 +1,11 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
 let dirpath = __dirname;
+let lastFolderSplit = dirpath.split( path.sep );
+let lastFolder = lastFolderSplit[ lastFolderSplit.length - 1 ];
+
+mix.sass('resources/theme/'+ lastFolder +'/src/scss/app.scss', 'public/theme/spmeca/css');
+mix.js('resources/theme/'+ lastFolder +'/src/js/app.js', 'public/theme/spmeca/js').vue({
+    version : 3
+});
