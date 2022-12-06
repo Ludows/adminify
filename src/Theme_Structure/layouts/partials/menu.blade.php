7ffa5c5a56319eabcbcfa8ea1a15ab2b6df32f52
Cointrel <cointrel@MacBook-Pro-de-Cointrel.local>
2022-12-06 18:51:55 +0100

    Add renderIf + beforerender to overwrite defaults

--- src/Theme_Structure/layouts/partials/menu.blade.php
+++ src/Theme_Structure/layouts/partials/menu.blade.php
@@ -7,35 +7,58 @@
     $link_class = isset($link_class) ? $link_class : 'nav-link';
     $active_class = isset($active_class) ? $active_class : 'active';
     $items = isset($items) ? $items : [];
+    $renderItem = true;
+    $beforeRender = function($item) {
+        return $item;
+    }
 @endphp
 
 <{!! $wrapper_tag !!} class="{!! $wrapper_class !!}">
 
     @foreach ($items as $item)
 
-        @if($item->type == 'custom')
-            <{!! $item_tag !!} class="{!! $item_class !!}">
-                <{!! $link_tag !!} class="{!! $link_class !!}" aria-current="page" target="{!! $item->open_new_tab == 0 ? '_self' : '_blank'  !!}" href="{{  $item->related->url }}">
-                    {{ empty($item->overwrite_title) ? $item->related->title : $item->overwrite_title }}
-                </{!! $link_tag !!}>
-            </{!! $item_tag !!}>
-            @if(!empty($item->childs))
-                @include('theme::'. $theme .'.layouts.partials.menu', [
-                    'items' => $item->childs
-                ])
-            @endif
+        @if(!empty($renderIf))
+            @php
+                $results_of_render = $renderIf($item);
+                if(is_bool($results_of_render)) {
+                    $renderItem = $results_of_render;
+                }
+            @endphp
+        @endif
 
-        @else
-            <{!! $item_tag !!} class="{!! $item_class !!}">
-                <{!! $link_tag !!} class="{!! $link_class !!} {!! $request->is( join('/', $item->related->url)) ? $active_class : ''  !!}" aria-current="page" target="{!! $item->open_new_tab == 0 ? '_self' : '_blank'  !!}" href="{{  $item->related->urlpath }}">
-                    {{ empty($item->overwrite_title) ? $item->related->title : $item->overwrite_title }}
-                </{!! $link_tag !!}>
-            </{!! $item_tag !!}>
+        @if($renderItem)
 
-            @if(!empty($item->childs))
-                @include('theme::'. $theme .'.layouts.partials.menu', [
-                    'items' => $item->childs
-                ])
+            @php
+                $result_of_item = $beforeRender($item);
+                if($result_of_item) {
+                    $item = $result_of_item;
+                }
+            @endphp
+        
+            @if($item->type == 'custom')
+                <{!! $item_tag !!} class="{!! $item_class !!}">
+                    <{!! $link_tag !!} class="{!! $link_class !!}" aria-current="page" target="{!! $item->open_new_tab == 0 ? '_self' : '_blank'  !!}" href="{{  $item->related->url }}">
+                        {{ empty($item->overwrite_title) ? $item->related->title : $item->overwrite_title }}
+                    </{!! $link_tag !!}>
+                </{!! $item_tag !!}>
+                @if(!empty($item->childs))
+                    @include('theme::'. $theme .'.layouts.partials.menu', [
+                        'items' => $item->childs
+                    ])
+                @endif
+
+            @else
+                <{!! $item_tag !!} class="{!! $item_class !!}">
+                    <{!! $link_tag !!} class="{!! $link_class !!} {!! $request->is( join('/', $item->related->url)) ? $active_class : ''  !!}" aria-current="page" target="{!! $item->open_new_tab == 0 ? '_self' : '_blank'  !!}" href="{{  $item->related->urlpath }}">
+                        {{ empty($item->overwrite_title) ? $item->related->title : $item->overwrite_title }}
+                    </{!! $link_tag !!}>
+                </{!! $item_tag !!}>
+
+                @if(!empty($item->childs))
+                    @include('theme::'. $theme .'.layouts.partials.menu', [
+                        'items' => $item->childs
+                    ])
+                @endif
             @endif
         @endif
 
