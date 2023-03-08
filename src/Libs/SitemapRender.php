b53e70c5b8bb9ee4e08ec01c9c93da25a0a5dfd5
Cointrel <cointrel@mbp-de-cointrel.home>
2023-03-08 15:19:47 +0100

    Add multilang switch for sitemaps

--- src/Libs/SitemapRender.php
+++ src/Libs/SitemapRender.php
@@ -105,9 +105,11 @@ class SitemapRender
             $single_sitemap = Sitemap::create(); 
             $isTranslatableModel = is_translatable_model($m);
             $isMultilang = $options['isMulti'];
+            $suffixMulti = '';
 
             if($isTranslatableModel && (bool) $isMultilang) {
                 $all = $m->lang($options['currentLang'])->get()->all();
+                $suffixMulti = '_'.$options['currentLang'];
             }
             else {
                 $all = $m->all()->all();
@@ -123,7 +125,7 @@ class SitemapRender
             }
 
 
-            $single_sitemap->writeToDisk('public', '/sitemap_'.$modelName.'.xml');
+            $single_sitemap->writeToDisk('public', '/sitemap_'.$modelName.$suffixMulti.'.xml');
         }
         
     }
@@ -140,10 +142,17 @@ class SitemapRender
     public function show() {
         $options = $this->getOptions();
         $hasKeys = array_keys($options['models']);
-        $fileToBind = 'sitemap_index.xml';
         $storage = storage('public');
+        $suffixMulti = '';
+
+        if($options['isMulti']) {
+            $suffixMulti = '_'.$options['currentLang'];
+        }
+
+        $fileToBind = 'sitemap_index'. $suffixMulti .'.xml';
+
         if(count($hasKeys) > 0) {
-            $fileToBind = 'sitemap_'. lowercase( plural( $hasKeys[0] ) ) .'.xml';
+            $fileToBind = 'sitemap_'. lowercase( plural( $hasKeys[0] ) ) .$suffixMulti.'.xml';
         }
 
         $fileExist = $storage->exists($fileToBind);
@@ -166,174 +175,27 @@ class SitemapRender
     public function checkRequiredExtend($model = null) {
         $isContentTypeModel = is_content_type_model($model);
         if(!$isContentTypeModel) {
-            throw new Exception('Are you sure that '.$modelName.' extends of Ludows\Adminify\Models\ContentTypeModel ?');
+            throw new Exception('Are you sure that the current model extends of Ludows\Adminify\Models\ContentTypeModel ?');
         }
     }
     public function createRootSitemap($options = []) {
        $root = SitemapIndex::create();
+
+       $suffixMulti = '';
+
+        if($options['isMulti']) {
+            $suffixMulti = '_'.$options['currentLang'];
+        }
+
+        $fileToBind = 'sitemap_index'. $suffixMulti .'.xml';
        
        foreach ($options['models'] as $modelName => $modelClass) {
             $m = new $modelClass();
             if($m->allowSitemap) {
-                $root->add('/sitemap_'.$modelName.'.xml');
+                $root->add('/sitemap_'.$modelName.$suffixMulti.'.xml');
             }
        }
        
-       $root->writeToDisk('public', '/sitemap_index.xml');
-    }
-
-
-    public function render() {
- // create new sitemap object
-        $sitemap = $this->sitemap;
-
-        $options = $this->getOptions();
-        $othersLangs = $this->langsExcludingCurrentLang($options['locales'], $options['currentLang']);
-        $isMultilang = get_site_key('multilang');
-
-        if($options['modelName'] == null && $options['writeFile'] == false) {
-                $sitemap->setCache('laravel.sitemap', 5);
-        }
-
-        foreach ($options['models'] as $modelName => $modelClass) {
-            # code...
-
-            if($options['modelName'] != null && $options['writeFile'] == false) {
-                $sitemap->setCache('laravel.sitemap.'.$options['modelName'], 5);
-            }
-            if (!$sitemap->isCached()) {
-
-                $traits = class_uses_recursive($modelClass);
-
-                if(!in_array('Ludows\Adminify\Traits\Sitemapable', $traits)) {
-                    throw new Exception($modelName.' must have the Ludows\Adminify\Traits\Sitemapable trait to work');
-                }
-
-                $m = new $modelClass();
-
-                $allowSitemap = $m->allowSitemap;
-                $isTranslatableModel = is_translatable_model($m);
-                $isContentTypeModel = is_content_type_model($m);
-                // $isLinkableMediaModel = is_linkable_media_model($m);
-
-                if(!$isContentTypeModel) {
-                    throw new Exception('Are you sure that '.$modelName.' extends of Ludows\Adminify\Models\ContentTypeModel ?');
-                }
-
-                if($allowSitemap) {
-                    if($isTranslatableModel && (bool) $isMultilang) {
-                        $all = $m->lang($options['currentLang'])->get()->all();
-                    }
-                    else {
-                        $all = $m->all()->all();
-                    }
-
-                    foreach ($all as $modelObject) {
-                        # code...
-                        //@to be continued
-                        $translations = [];
-                        $images = [];
-                        // check translated pages..
-                        if($isMultilang && $isTranslatableModel) {
-                            foreach ($othersLangs as $l) {
-                                # code...
-                                $transations = $m->getTranslations();
-
-                                foreach ($translations as $t) {
-                                    # code...
-                                    if(array_key_exists($l, $t)) {
-
-                                        //get url according the locale setted for the loop.
-                                        app()->setLocale($l);
-
-                                        $translations[] = [
-                                            'language' => $l,
-                                            'url' => $modelObject->urlpath
-                                        ];
-
-                                        break;
-                                    }
-                                }
-                            }
-                            //restore the current locale
-                            app()->setLocale($options['currentLang']);
-                        }
-
-                        if (Schema::hasColumn($modelObject->getTable(), $modelObject->media_key)) {
-
-                            // column exist
-                            $imageSources = $modelObject->getSitemapImages();
-
-                            foreach ($imageSources as $imageSource) {
-                                # code...
-                                $pathable_media = $imageSource->path;
-                                $pathInfo = pathinfo($pathable_media);
-                                $imageInformations = [
-                                    'url' => $pathable_media,
-                                    'title' => $pathInfo['filename']
-                                ];
-
-                                if(!empty($imageSource->alt)) {
-                                    $imageInformations['caption'] = $imageSource->alt;
-                                }
-
-                                $images[] = $imageInformations;
-
-                            }
-                        }
-
-
-                        // if($isLinkableMediaModel) {
-                        //     $media = $modelObject->media;
-                        //     if($media != null) {
-                        //         $pathInfo = pathinfo($media->path);
-                        //         $imageInformations = [
-                        //             'url' => $media->path,
-                        //             'title' => $pathInfo['filename']
-                        //         ];
-
-                        //         if($media->alt && strlen($media->alt) > 0) {
-                        //             $imageInformations['caption'] = $media->alt;
-                        //         }
-
-                        //         $images[] = $imageInformations;
-                        //     }
-                        // }
-
-                        //$loc, $lastmod = null, $priority = null, $freq = null, $images = [], $title = null, $translations = [], $videos = [], $googlenews = [], $alternates = []
-                        // $fullpath = $modelObject->urlpath;
-                        // if($fullpath != null) {
-
-                        //     if(!is_homepage($m) && $isUrlableModel) {
-                        //         $path = $modelObject->urlpath;
-                        //     }
-                        //     else {
-                        //         $path = $modelObject->{$modelObject->sitemapCallable};
-                        //     }
-
-                        //     if(is_homepage($m) && $isUrlableModel) {
-                        //         $path = '/';
-                        //     }
-
-                        //     $sitemap->add($path, $modelObject->updated_at, '0.9', 'monthly', $images, $modelObject->sitemapTitle, $translations);
-                        // }
-
-                        $url = $modelObject->getSitemapUrl();
-                        $sitemap->add($url, $modelObject->updated_at, $modelObject->priority_sitemap, $modelObject->freq_sitemap, $images, $modelObject->sitemapTitle, $translations, [], [], $translations);
-
-                    }
-
-                    if($options['writeFile'] && $options['modelName'] != null) {
-                        $sitemap->store('xml', $modelName.'-sitemap');
-                        // $this->info('the sitemap '. $modelName .'-sitemap.xml was generated');
-                    }
-                }
-            }
-
-        }
-
-        //dd($sitemap);
-
-        return $options['writeFile']  ? 0 : $sitemap->render('xml');
+       $root->writeToDisk('public', '/'.$fileToBind);
     }
 }
